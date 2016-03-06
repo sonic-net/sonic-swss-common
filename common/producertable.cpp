093a82826a80e97c913410ad5a016205214a6796
Elad Raz <e@eladraz.com>
2016-03-06 16:13:26 +0200

    common: Adding ProducerTable class
    The ProducerTable class implements SET and DEL API for Redis Table.
    
    SET is implemented via "HSET" command to add the list of fields and
    values into Redis DB.
    DEL uses Redis "DEL" command.
    
    In addition, the producer-table implement the HSET in a transaction
    using "MULTI" and "EXEC" commands.
    
    In addition it adds OP, VALUE and KEY elements into the tracking
    list and trigger a SUBSCRIBE to wake up all listeners
    
    Signed-off-by: Elad Raz <eladr@mellanox.com>

---
+++ common/producertable.cpp
@@ -0,0 +1,76 @@
+#include "common/redisreply.h"
+#include "common/producertable.h"
+#include "common/json.h"
+#include <stdlib.h>
+#include <tuple>
+
+using namespace std;
+
+namespace swss {
+
+ProducerTable::ProducerTable(DBConnector *db, string tableName) :
+    Table(db, tableName)
+{
+}
+
+void ProducerTable::enqueueDbChange(string key, string value, string op)
+{
+    string lpush_value;
+    string lpush_key("LPUSH ");
+    string lpush_op = lpush_key;
+    char *temp;
+    int len;
+
+    lpush_key += getKeyQueueTableName();
+    lpush_key += " ";
+    lpush_key += key;
+    enqueue(lpush_key, REDIS_REPLY_INTEGER);
+
+    len = redisFormatCommand(&temp, "LPUSH %s %s", getValueQueueTableName().c_str(), value.c_str());
+    lpush_value = string(temp, len);
+    free(temp);
+    enqueue(lpush_value, REDIS_REPLY_INTEGER, true);
+
+    lpush_op += getOpQueueTableName();
+    lpush_op += " ";
+    lpush_op += op;
+    enqueue(lpush_op, REDIS_REPLY_INTEGER);
+
+    string publish("PUBLISH ");
+    publish += getChannelTableName();
+    publish += " G";
+    enqueue(publish, REDIS_REPLY_INTEGER);
+}
+
+void ProducerTable::set(string key, vector<FieldValueTuple> &values, string op)
+{
+    multi();
+    for (FieldValueTuple &i : values)
+    {
+        char *temp;
+        int len = redisFormatCommand(&temp, "HSET %s %s %s",
+                                     getKeyName(key).c_str(),
+                                     fvField(i).c_str(),
+                                     fvValue(i).c_str());
+        string hset(temp, len);
+        free(temp);
+        enqueue(hset, REDIS_REPLY_INTEGER, true);
+    }
+    enqueueDbChange(key, JSon::buildJson(values), op);
+    exec();
+}
+
+void ProducerTable::del(string key, string op)
+{
+    string del("DEL ");
+    del += getKeyName(key);
+
+    multi();
+
+    enqueueDbChange(key, "{}", op);
+    enqueue(del, REDIS_REPLY_INTEGER);
+
+    exec();
+}
+
+}
---
+++ common/producertable.h
@@ -0,0 +1,33 @@
+#ifndef __PRODUCERTABLE__
+#define __PRODUCERTABLE__
+
+#include <string>
+#include <vector>
+
+#include <hiredis/hiredis.h>
+#include "common/dbconnector.h"
+#include "common/table.h"
+#include "common/scheme.h"
+
+namespace swss {
+
+class ProducerTable : public Table
+{
+public:
+    ProducerTable(DBConnector *db, std::string tableName);
+
+    void set(std::string key, std::vector<FieldValueTuple> &values,
+             std::string op = SET_COMMAND);
+    void del(std::string key, std::string op = DEL_COMMAND);
+
+private:
+    /* Disable copy-constructor and operator = */
+    ProducerTable(const ProducerTable &other);
+    ProducerTable & operator = (const ProducerTable &other);
+
+    void enqueueDbChange(std::string key, std::string value, std::string op);
+};
+
+}
+
+#endif
