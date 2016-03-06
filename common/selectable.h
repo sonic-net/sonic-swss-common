347ba0adc294b6458611e552835d93db0a16595b
Elad Raz <e@eladraz.com>
2016-03-06 16:21:09 +0200

    common: Adding Selectable interface
    The Selectable interface allow selecting ("select (2)") of a class. This
    interface allows the following operations:
    * Reading cached information (For example Redis cache)
    * Adding file descriptor(s) to fd_set
    * Checking if the file desriptor matches on a class
    * Reading the packet
    
    Signed-off-by: Elad Raz <eladr@mellanox.com>

---
+++ common/selectable.h
@@ -0,0 +1,37 @@
+#ifndef __SELECT__
+#define __SELECT__
+
+#include <string>
+#include <vector>
+#include <limits>
+#include <hiredis/hiredis.h>
+#include "common/dbconnector.h"
+#include "common/table.h"
+
+namespace swss {
+
+class Selectable
+{
+public:
+    virtual ~Selectable() {};
+
+    enum {
+        DATA = 0,
+        ERROR = 1,
+        NODATA = 2
+    };
+
+    /* Implements FD_SET */
+    virtual void addFd(fd_set *fd) = 0;
+    virtual bool isMe(fd_set *fd) = 0;
+
+    /* Read and empty socket caching (if exists) */
+    virtual int readCache() = 0;
+
+    /* Read a message from the socket */
+    virtual void readMe() = 0;
+};
+
+}
+
+#endif
