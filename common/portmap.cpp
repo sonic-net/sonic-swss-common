89b80bcabeedc6d2011603a2386e799b75976298
Shuotian Cheng <stcheng_89@hotmail.com>
2016-07-26 16:00:51 -0700

    portmap: Adding handlePortMap function to read port_config.ini (#29)

---
+++ common/portmap.cpp
@@ -0,0 +1,35 @@
+#include "portmap.h"
+
+using namespace std;
+
+map<set<int>, string> handlePortMap(string file)
+{
+    map<set<int>, string> port_map;
+
+    ifstream infile(file);
+    if (!infile.is_open())
+        throw "Cannot open port map configuration file!";
+
+    string line;
+    while (getline(infile, line))
+    {
+        if (line.at(0) == '#')
+            continue;
+
+        istringstream iss_line(line);
+        string alias, lanes, lane;
+        set<int> lane_set;
+
+        iss_line >> alias >> lanes;
+        istringstream iss_lane(lanes);
+
+        while (getline(iss_lane, lane, ','))
+            lane_set.insert(stoi(lane));
+
+        port_map[lane_set] = alias;
+    }
+
+    infile.close();
+
+    return port_map;
+}
