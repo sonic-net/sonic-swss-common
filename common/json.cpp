7673a7d0c9a3747d15ee1e2294f2cbbc29d1b7f6
Kamil Cudnik <kcudnik@microsoft.com>
2016-10-21 10:56:52 -0700

    Use json.hpp as json lib

--- common/json.cpp
+++ common/json.cpp
@@ -1,4 +1,5 @@
 #include "common/json.h"
+#include "common/json.hpp"
 #include <sstream>
 #include <limits>
 
@@ -6,75 +7,30 @@ using namespace std;
 
 namespace swss {
 
-static const string gHex = "0123456789ABCDEF";
-static bool shouldEncode(char ch)
-{
-    return ch == '{' || ch == '}' || ch == '\"' || ch == '\'' || ch == '%';
-}
-
-static string escape(const string &a)
-{
-    string ret;
-    for (char i : a)
-    {
-        if (shouldEncode(i))
-            ret += string("%") + gHex[i >> 4] + gHex[i & 0xF];
-        else
-            ret += i;
-    }
-    return ret;
-}
-
-static string descape(const string &a)
-{
-    string ret;
-    for (auto i = a.begin(); i != a.end(); i++)
-    {
-        if (*i != '%')
-        {
-            ret+= *i;
-            continue;
-        }
-
-        i++;
-        ret += char(((gHex.find(*i++)) << 4) | gHex.find(*i));
-    }
-    return ret;
-}
-
 string JSon::buildJson(const vector<FieldValueTuple> &fv)
 {
-    string tab = "  ";
-    string json = "{" + tab;
+    nlohmann::json j;
 
-    //for (FieldValueTuple &i : fv)
-    for (auto i = fv.begin(); i != fv.end();)
+    for (auto &i : fv)
     {
-        json+= tab + "\"" + escape(fvField(*i)) + "\" : \"" +
-               escape(fvValue(*i)) + "\"";
-        if (++i != fv.end())
-            json += "," + tab;
+        j[fvField(i)] = fvValue(i);
     }
-    json += tab + "}";
-    return json;
+
+    return j.dump();
 }
 
-void JSon::readJson(const string &json, vector<FieldValueTuple> &fv)
+void JSon::readJson(const string &jsonstr, vector<FieldValueTuple> &fv)
 {
-    istringstream f(json);
-    string field, value, tmp;
+    nlohmann::json j = nlohmann::json::parse(jsonstr);
+
     FieldValueTuple e;
 
-    /* Read the spaces and charactes until { "field" = "value", */
-    while (getline(f, tmp, '\"')) {
-        /* Read field name */
-        if (!getline(f, field, '\"')) break;
-        fvField(e) = descape(field);
-        /* Read " = " */
-        if (!getline(f, tmp, '\"')) break;
-        /* Read value name */
-        if (!getline(f, value, '\"')) break;
-        fvValue(e) = descape(value);
+    std::map<std::string, nlohmann::json> m = j;
+
+    for (auto&& kv : m)
+    {
+        fvField(e) = kv.first;
+        fvValue(e) = kv.second;
         fv.push_back(e);
     }
 }
