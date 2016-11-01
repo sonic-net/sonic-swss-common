b00a38ead8d1a6ec54d3653deb2f6ab926647348
Shuotian Cheng <shuche@microsoft.com>
2016-10-31 17:28:07 -0700

    IpAddress: Adding getV6Addr() function (#56)

--- common/ipaddress.h
+++ common/ipaddress.h
@@ -47,6 +47,11 @@ public:
         return m_ip.ip_addr.ipv4_addr;
     }
 
+    inline const unsigned char* getV6Addr() const
+    {
+        return m_ip.ip_addr.ipv6_addr;
+    }
+
     inline bool operator<(const IpAddress &o) const
     {
         if (m_ip.family != o.m_ip.family)
