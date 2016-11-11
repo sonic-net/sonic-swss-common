8b25472e17394ee27329d2857a7c878770653fdf
Kamil Cudnik <kcudnik@microsoft.com>
2016-11-11 11:30:46 -0800

    Fix arguments in log message

--- common/redisapi.h
+++ common/redisapi.h
@@ -15,7 +15,7 @@ static inline std::string loadRedisScript(DBConnector* db, const std::string& sc
 
     std::string sha = r.getReply<std::string>();
 
-    SWSS_LOG_NOTICE("lua script loaded, sha: %s", sha.c_str(), script.c_str());
+    SWSS_LOG_NOTICE("lua script loaded, sha: %s", sha.c_str());
     SWSS_LOG_INFO("lua script: %s", script.c_str());
 
     return sha;
