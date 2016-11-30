3bfefac86cf66ef63f5a67c18de4de0fa73511b7
Qi Luo <qiluo-msft@users.noreply.github.com>
2016-11-30 02:12:57 +0000

    Load lua script only once

--- common/producerstatetable.cpp
+++ common/producerstatetable.cpp
@@ -14,6 +14,21 @@ ProducerStateTable::ProducerStateTable(DBConnector *db, std::string tableName)
     , m_db(db)
     , m_pipe(new RedisPipeline(db))
 {
+    std::string luaSet =
+        "redis.call('SADD', KEYS[2], ARGV[2])\n"
+        "for i = 0, #KEYS - 3 do\n"
+        "    redis.call('HSET', KEYS[3 + i], ARGV[3 + i * 2], string.sub(ARGV[4 + i * 2], 2, -1))\n"
+        "end\n"
+        "redis.call('PUBLISH', KEYS[1], ARGV[1])\n";
+
+    shaSet = loadRedisScript(m_db, luaSet);
+
+    std::string luaDel =
+        "redis.call('SADD', KEYS[2], ARGV[2])\n"
+        "redis.call('DEL', KEYS[3])\n"
+        "redis.call('PUBLISH', KEYS[1], ARGV[1])\n";
+
+    shaDel = loadRedisScript(m_db, luaDel);
 }
 
 ProducerStateTable::~ProducerStateTable()
@@ -25,15 +40,6 @@ ProducerStateTable::~ProducerStateTable()
 task ProducerStateTable::setAsync(std::string key, std::vector<FieldValueTuple> &values,
                  std::string op /*= SET_COMMAND*/)
 {
-    static std::string luaSet =
-        "redis.call('SADD', KEYS[2], ARGV[2])\n"
-        "for i = 0, #KEYS - 3 do\n"
-        "    redis.call('HSET', KEYS[3 + i], ARGV[3 + i * 2], string.sub(ARGV[4 + i * 2], 2, -1))\n"
-        "end\n"
-        "redis.call('PUBLISH', KEYS[1], ARGV[1])\n";
-
-    shaSet = loadRedisScript(m_db, luaSet);
-
     std::ostringstream osk, osv;
     osk << "EVALSHA "
         << shaSet << ' '
@@ -64,13 +70,6 @@ void ProducerStateTable::set(std::string key, std::vector<FieldValueTuple> &valu
 
 task ProducerStateTable::delAsync(std::string key, std::string op /*= DEL_COMMAND*/)
 {
-    std::string luaDel =
-        "redis.call('SADD', KEYS[2], ARGV[2])\n"
-        "redis.call('DEL', KEYS[3])\n"
-        "redis.call('PUBLISH', KEYS[1], ARGV[1])\n";
-
-    shaDel = loadRedisScript(m_db, luaDel);
-
     std::ostringstream osk, osv;
     osk << "EVALSHA "
         << shaDel << ' '
