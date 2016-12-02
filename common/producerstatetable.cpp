ae73e18575d4d9bb31346949690b514f5075b385
Qi Luo <qiluo-msft@users.noreply.github.com>
2016-12-02 00:37:50 +0000

    Remvoe std::

--- common/producerstatetable.cpp
+++ common/producerstatetable.cpp
@@ -11,23 +11,23 @@ using namespace std;
 
 namespace swss {
 
-ProducerStateTable::ProducerStateTable(DBConnector *db, std::string tableName)
+ProducerStateTable::ProducerStateTable(DBConnector *db, string tableName)
     : TableName_KeySet(tableName)
     , m_db(db)
 {
 }
 
-void ProducerStateTable::set(std::string key, std::vector<FieldValueTuple> &values,
-                 std::string op /*= SET_COMMAND*/, std::string prefix)
+void ProducerStateTable::set(string key, vector<FieldValueTuple> &values,
+                 string op /*= SET_COMMAND*/, string prefix)
 {
-    static std::string luaScript =
+    static string luaScript =
         "redis.call('SADD', KEYS[2], ARGV[2])\n"
         "for i = 0, #KEYS - 3 do\n"
         "    redis.call('HSET', KEYS[3 + i], ARGV[3 + i * 2], ARGV[4 + i * 2])\n"
         "end\n"
         "redis.call('PUBLISH', KEYS[1], ARGV[1])\n";
 
-    static std::string sha = loadRedisScript(m_db, luaScript);
+    static string sha = loadRedisScript(m_db, luaScript);
 
     // Assembly redis command args into a string vector
     vector<string> args;
@@ -50,7 +50,7 @@ void ProducerStateTable::set(std::string key, std::vector<FieldValueTuple> &valu
 
     // Transform data structure
     vector<const char *> args1;
-    transform(args.begin(), args.end(), std::back_inserter(args1), [](const string s) { return s.c_str(); } ); 
+    transform(args.begin(), args.end(), back_inserter(args1), [](const string s) { return s.c_str(); } ); 
     
     // Invoke redis command
     RedisCommand command;
@@ -58,14 +58,14 @@ void ProducerStateTable::set(std::string key, std::vector<FieldValueTuple> &valu
     RedisReply r(m_db, command, REDIS_REPLY_NIL);
 }
 
-void ProducerStateTable::del(std::string key, std::string op /*= DEL_COMMAND*/, std::string prefix)
+void ProducerStateTable::del(string key, string op /*= DEL_COMMAND*/, string prefix)
 {
-    static std::string luaScript =
+    static string luaScript =
         "redis.call('SADD', KEYS[2], ARGV[2])\n"
         "redis.call('DEL', KEYS[3])\n"
         "redis.call('PUBLISH', KEYS[1], ARGV[1])\n";
 
-    static std::string sha = loadRedisScript(m_db, luaScript);
+    static string sha = loadRedisScript(m_db, luaScript);
     
     // Assembly redis command args into a string vector
     vector<string> args;
@@ -81,7 +81,7 @@ void ProducerStateTable::del(std::string key, std::string op /*= DEL_COMMAND*/, 
 
     // Transform data structure
     vector<const char *> args1;
-    transform(args.begin(), args.end(), std::back_inserter(args1), [](const string s) { return s.c_str(); } ); 
+    transform(args.begin(), args.end(), back_inserter(args1), [](const string s) { return s.c_str(); } ); 
     
     // Invoke redis command
     RedisCommand command;
