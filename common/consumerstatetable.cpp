9b8dd934a315aeef5ae3bc4db27f897f559d620c
Qi Luo <qiluo-msft@users.noreply.github.com>
2016-12-07 17:27:43 -0800

    Implement buffered pop for ConsumerTable/ConsumerStateTable (#74)
    * Implement ConsumerStateTable::pops
    
    * Make pop size as class constance
    
    * Remove pops interface
    
    * Implement buffered pop for ConsumerTable
    
    * (reformat)

--- common/consumerstatetable.cpp
+++ common/consumerstatetable.cpp
@@ -1,5 +1,5 @@
 #include <string>
-#include <vector>
+#include <deque>
 #include <limits>
 #include <hiredis/hiredis.h>
 #include "dbconnector.h"
@@ -14,6 +14,7 @@ namespace swss {
 ConsumerStateTable::ConsumerStateTable(DBConnector *db, std::string tableName)
     : RedisTransactioner(db)
     , TableName_KeySet(tableName)
+    , POP_BATCH_SIZE(128)
 {
     for (;;)
     {
@@ -32,60 +33,91 @@ ConsumerStateTable::ConsumerStateTable(DBConnector *db, std::string tableName)
 
 void ConsumerStateTable::pop(KeyOpFieldsValuesTuple &kco, std::string prefix)
 {
-    auto& values = kfvFieldsValues(kco);
-    values.clear();
+    if (m_buffer.empty())
+    {
+        pops(m_buffer, prefix);
+    }
+    
+    if (m_buffer.empty())
+    {
+        auto& values = kfvFieldsValues(kco);
+        values.clear();
+        kfvKey(kco).clear();
+        kfvOp(kco).clear();
+        return;
+    }
+    
+    kco = m_buffer.front();
+    m_buffer.pop_front();
+}
 
+void ConsumerStateTable::pops(std::deque<KeyOpFieldsValuesTuple> &vkco, std::string /*prefix*/)
+{
     static std::string luaScript =
-        "local key = redis.call('SPOP', KEYS[1])\n"
-        "if not key then return end\n"
-        "local values = redis.call('HGETALL', KEYS[2] .. key)\n"
-        "local ret = {key, values}\n"
+        "local ret = {}\n"
+        "local keys = redis.call('SPOP', KEYS[1], ARGV[1])\n"
+        "local n = table.getn(keys)\n"
+        "for i = 1, n do\n"
+            "local key = keys[i]\n"
+            "local values = redis.call('HGETALL', KEYS[2] .. key)\n"
+            "table.insert(ret, {key, values})\n"
+        "end\n"
         "return ret\n";
 
     static std::string sha = loadRedisScript(m_db, luaScript);
 
     RedisCommand command;
     command.format(
-        "EVALSHA %s 2 %s %s: '' '' ''",
+        "EVALSHA %s 2 %s %s: %d ''",
         sha.c_str(),
         getKeySetName().c_str(),
-        getTableName().c_str());
+        getTableName().c_str(),
+        POP_BATCH_SIZE);
 
     RedisReply r(m_db, command);
-    auto ctx = r.getContext();
+    auto ctx0 = r.getContext();
+    vkco.clear();
 
     // if the set is empty, return an empty kco object
-    if (r.getContext()->type == REDIS_REPLY_NIL)
+    if (ctx0->type == REDIS_REPLY_NIL)
     {
-        kfvKey(kco).clear();
-        kfvOp(kco).clear();
         return;
     }
 
-    assert(ctx->type == REDIS_REPLY_ARRAY);
-    assert(ctx->elements == 2);
-    assert(ctx->element[0]->type == REDIS_REPLY_STRING);
-    std::string key = ctx->element[0]->str;
-    kfvKey(kco) = key;
+    assert(ctx0->type == REDIS_REPLY_ARRAY);
+    size_t n = ctx0->elements;
+    vkco.resize(n);
+    for (size_t ie = 0; ie < n; ie++)
+    {
+        auto& kco = vkco[ie];
+        auto& values = kfvFieldsValues(kco);
+        assert(values.empty());
 
-    assert(ctx->element[1]->type == REDIS_REPLY_ARRAY);
-    auto ctx1 = ctx->element[1];
-    for (size_t i = 0; i < ctx1->elements / 2; i++)
-    {
-        FieldValueTuple e;
-        fvField(e) = ctx1->element[i * 2]->str;
-        fvValue(e) = ctx1->element[i * 2 + 1]->str;
-        values.push_back(e);
-    }
+        auto& ctx = ctx0->element[ie];
+        assert(ctx->elements == 2);
+        assert(ctx->element[0]->type == REDIS_REPLY_STRING);
+        std::string key = ctx->element[0]->str;
+        kfvKey(kco) = key;
 
-    // if there is no field-value pair, the key is already deleted
-    if (values.empty())
-    {
-        kfvOp(kco) = DEL_COMMAND;
-    }
-    else
-    {
-        kfvOp(kco) = SET_COMMAND;
+        assert(ctx->element[1]->type == REDIS_REPLY_ARRAY);
+        auto ctx1 = ctx->element[1];
+        for (size_t i = 0; i < ctx1->elements / 2; i++)
+        {
+            FieldValueTuple e;
+            fvField(e) = ctx1->element[i * 2]->str;
+            fvValue(e) = ctx1->element[i * 2 + 1]->str;
+            values.push_back(e);
+        }
+
+        // if there is no field-value pair, the key is already deleted
+        if (values.empty())
+        {
+            kfvOp(kco) = DEL_COMMAND;
+        }
+        else
+        {
+            kfvOp(kco) = SET_COMMAND;
+        }
     }
 }
 
--- common/consumertable.cpp
+++ common/consumertable.cpp
@@ -17,6 +17,7 @@ namespace swss {
 ConsumerTable::ConsumerTable(DBConnector *db, string tableName)
     : RedisTransactioner(db)
     , TableName_KeyValueOpQueues(tableName)
+    , POP_BATCH_SIZE(128)
 {
     for (;;)
     {
@@ -35,81 +36,126 @@ ConsumerTable::ConsumerTable(DBConnector *db, string tableName)
 
 void ConsumerTable::pop(KeyOpFieldsValuesTuple &kco, string prefix)
 {
+    if (m_buffer.empty())
+    {
+        pops(m_buffer, prefix);
+    }
+
+    if (m_buffer.empty())
+    {
+        auto& values = kfvFieldsValues(kco);
+        values.clear();
+        kfvKey(kco).clear();
+        kfvOp(kco).clear();
+        return;
+    }
+
+    kco = m_buffer.front();
+    m_buffer.pop_front();
+}
+
+void ConsumerTable::pops(deque<KeyOpFieldsValuesTuple> &vkco, string prefix)
+{
     static string luaScript =
+        "local rets = {}\n"
+        "local keys   = redis.call('LRANGE', KEYS[1], -ARGV[1], -1)\n"
+        "local ops    = redis.call('LRANGE', KEYS[2], -ARGV[1], -1)\n"
+        "local values = redis.call('LRANGE', KEYS[3], -ARGV[1], -1)\n"
+        "redis.call('LTRIM', KEYS[1], 0, -ARGV[1]-1)"
+        "redis.call('LTRIM', KEYS[2], 0, -ARGV[1]-1)"
+        "redis.call('LTRIM', KEYS[3], 0, -ARGV[1]-1)"
 
-        "local key   = redis.call('RPOP', KEYS[1])\n"
-        "local op    = redis.call('RPOP', KEYS[2])\n"
-        "local value = redis.call('RPOP', KEYS[3])\n"
+        "local n = table.getn(keys)\n"
+        "for i = n, 1, -1 do\n"
+            "local key = keys[i]\n"
+            "local op = ops[i]\n"
+            "local value = values[i]\n"
+            "local dbop = op:sub(1,1)\n"
+            "op = op:sub(2)\n"
+            "local ret = {key, op}\n"
 
-        "local dbop = op:sub(1,1)\n"
-        "op = op:sub(2)\n"
-        "local ret = {key, op}\n"
+            "local jj = cjson.decode(value)\n"
+            "local size = #jj\n"
 
-        "local jj = cjson.decode(value)\n"
-        "local size = #jj\n"
+            "for idx=1,size,2 do\n"
+            "    table.insert(ret, jj[idx])\n"
+            "    table.insert(ret, jj[idx+1])\n"
+            "end\n"
+            "table.insert(rets, ret)\n"
 
-        "for idx=1,size,2 do\n"
-        "    table.insert(ret, jj[idx])\n"
-        "    table.insert(ret, jj[idx+1])\n"
+            "if op ~= 'get' and op ~= 'getresponse' and op ~= 'notify' then\n"
+                "local keyname = KEYS[4] .. ':' .. key\n"
+                "if key == '' then\n"
+                "   keyname = KEYS[4]\n"
+                "end\n"
+
+                "if dbop == 'D' then\n"
+                "   redis.call('DEL', keyname)\n"
+                "else\n"
+                "   local st = 3\n"
+                "   local len = #ret\n"
+                "   while st <= len do\n"
+                "       redis.call('HSET', keyname, ret[st], ret[st+1])\n"
+                "       st = st + 2\n"
+                "   end\n"
+                "end\n"
+            "end\n"
         "end\n"
-
-        "if op == 'get' or op == 'getresponse' or op == 'notify' then\n"
-        "return ret\n"
-        "end\n"
-
-        "local keyname = KEYS[4] .. ':' .. key\n"
-        "if key == '' then\n"
-        "   keyname = KEYS[4]\n"
-        "end\n"
-
-        "if dbop == 'D' then\n"
-        "   redis.call('DEL', keyname)\n"
-        "else\n"
-        "   local st = 3\n"
-        "   local len = #ret\n"
-        "   while st <= len do\n"
-        "       redis.call('HSET', keyname, ret[st], ret[st+1])\n"
-        "       st = st + 2\n"
-        "   end\n"
-        "end\n"
-
-        "return ret";
+        "return rets";
 
     static string sha = loadRedisScript(m_db, luaScript);
     RedisCommand command;
     command.format(
-        "EVALSHA %s 4 %s %s %s %s '' '' '' ''",
+        "EVALSHA %s 4 %s %s %s %s %d '' '' ''",
         sha.c_str(),
         getKeyQueueTableName().c_str(),
         getOpQueueTableName().c_str(),
         getValueQueueTableName().c_str(),
-        (prefix+getTableName()).c_str());
+        (prefix+getTableName()).c_str(),
+        POP_BATCH_SIZE);
 
     RedisReply r(m_db, command, REDIS_REPLY_ARRAY);
 
-    auto ctx = r.getContext();
+    auto ctx0 = r.getContext();
+    vkco.clear();
 
-    string key = ctx->element[0]->str;
-    string op  = ctx->element[1]->str;
-
-    vector<FieldValueTuple> fieldsValues;
-
-    for (size_t i = 2; i < ctx->elements; i += 2)
+    // if the set is empty, return an empty kco object
+    if (r.getContext()->type == REDIS_REPLY_NIL)
     {
-        if (i+1 >= ctx->elements)
-        {
-            SWSS_LOG_ERROR("invalid number of elements in returned table: %lu >= %lu", i+1, ctx->elements);
-            throw runtime_error("invalid number of elements in returned table");
-        }
-
-        FieldValueTuple e;
-
-        fvField(e) = ctx->element[i+0]->str;
-        fvValue(e) = ctx->element[i+1]->str;
-        fieldsValues.push_back(e);
+        return;
     }
 
-    kco = make_tuple(key, op, fieldsValues);
+    assert(ctx0->type == REDIS_REPLY_ARRAY);
+    size_t n = ctx0->elements;
+    vkco.resize(n);
+
+    for (size_t ie = 0; ie < n; ie++)
+    {
+        auto& kco = vkco[ie];
+        auto& values = kfvFieldsValues(kco);
+        assert(values.empty());
+
+        auto& ctx = ctx0->element[ie];
+        string key = ctx->element[0]->str;
+        kfvKey(kco) = key;
+        string op  = ctx->element[1]->str;
+        kfvOp(kco) = op;
+
+        for (size_t i = 2; i < ctx->elements; i += 2)
+        {
+            if (i+1 >= ctx->elements)
+            {
+                SWSS_LOG_ERROR("invalid number of elements in returned table: %lu >= %lu", i+1, ctx->elements);
+                throw runtime_error("invalid number of elements in returned table");
+            }
+
+            FieldValueTuple e;
+
+            fvField(e) = ctx->element[i+0]->str;
+            fvValue(e) = ctx->element[i+1]->str;
+            values.push_back(e);
+        }
+    }
 }
 
 }
