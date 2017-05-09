5e24febc2a07f7f1dd0baea3c0318027e5351a02
Shuotian Cheng <shuche@microsoft.com>
2017-05-08 17:25:38 -0700

    [test]: Add MacAddress test
    Signed-off-by: Shuotian Cheng <shuche@microsoft.com>

---
+++ tests/macaddress_ut.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+#include "common/macaddress.h"
+
+using namespace swss;
+
+TEST(MacAddress, to_string)
+{
+    auto a = MacAddress("52:54:00:ac:3a:99");
+    auto b = MacAddress("02:09:87:65:43:21");
+
+    EXPECT_EQ(a.to_string(), "52:54:00:ac:3a:99");
+    EXPECT_EQ(b.to_string(), "02:09:87:65:43:21");
+}
+
+TEST(MacAddress, comparison)
+{
+    auto a = MacAddress("52:54:00:ac:3a:99");
+    auto b = MacAddress("02:09:87:65:43:21");
+
+    EXPECT_EQ(a == a, true);
+    EXPECT_EQ(a != a, false);
+
+    EXPECT_EQ(a == b, false);
+    EXPECT_EQ(a != b, true);
+
+    EXPECT_LT(b, a);
+
+    EXPECT_EQ(a < b, false);
+    EXPECT_EQ(b < a, true);
+
+    EXPECT_EQ(!a, false);
+    EXPECT_EQ(!b, false);
+
+    auto c = MacAddress("00:00:00:00:00:00");
+
+    EXPECT_EQ(!c, true);
+}
