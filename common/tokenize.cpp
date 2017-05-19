91b425b18059ca173f0084ca35e75568688ecedb
Shuotian Cheng <shuche@microsoft.com>
2017-05-19 00:08:28 -0700

    [tokenize]: Add tokenize(..., const size_t firstN) function (#96)
    This function is helpful when trying to tokenize a key-value string
    that the tokenizer is used between hierarchical keys and values and/or
    values that also contain the tokenizer.
    
    e.g.
    tokenize("NEIGH_TABLE:lo:fc00::79", ':', 2) -> ['NEIGH_TABLE', 'lo', 'fc00::79']

--- common/tokenize.cpp
+++ common/tokenize.cpp
@@ -15,5 +15,23 @@ vector<string> tokenize(const string &str, const char token)
 
     return ret;
 }
+vector<string> tokenize(const string &str, const char token, const size_t firstN)
+{
+    vector<string> ret;
+    string tmp = str;
+    size_t i = 0;
+    auto pos = tmp.find(token);
+
+    while (pos != string::npos && i++ < firstN)
+    {
+        ret.push_back(tmp.substr(0, pos));
+        tmp = tmp.substr(pos+1);
+        pos = tmp.find(token);
+    }
+
+    ret.push_back(tmp);
+
+    return ret;
+}
 
 }
