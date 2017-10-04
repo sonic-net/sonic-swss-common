7afd10a01bc57e1f3d2ebc54c22eb20d5c7243a6
Shu0T1an ChenG <shuche@microsoft.com>
2017-10-03 21:08:39 -0700

    [schema]: Add init state database entries

--- common/schema.h
+++ common/schema.h
@@ -3,6 +3,8 @@
 
 namespace swss {
 
+/***** DATABASE *****/
+
 #define APPL_DB         0
 #define ASIC_DB         1
 #define COUNTERS_DB     2
@@ -10,6 +12,8 @@ namespace swss {
 #define CONFIG_DB       4
 #define PFC_WD_DB       5
 #define STATE_DB        6
+
+/***** APPLICATION DATABASE *****/
 
 #define APP_PORT_TABLE_NAME             "PORT_TABLE"
 #define APP_VLAN_TABLE_NAME             "VLAN_TABLE"
@@ -24,6 +28,7 @@ namespace swss {
 #define APP_ACL_TABLE_NAME              "ACL_TABLE"
 #define APP_ACL_RULE_TABLE_NAME         "ACL_RULE_TABLE"
 #define APP_FDB_TABLE_NAME              "FDB_TABLE"
+#define APP_PFC_WD_TABLE_NAME           "PFC_WD_TABLE"
 #define APP_SWITCH_TABLE_NAME           "SWITCH_TABLE"
 
 #define APP_TC_TO_QUEUE_MAP_TABLE_NAME  "TC_TO_QUEUE_MAP_TABLE"
@@ -43,6 +48,8 @@ namespace swss {
 #define APP_PFC_PRIORITY_TO_PRIORITY_GROUP_MAP_NAME "PFC_PRIORITY_TO_PRIORITY_GROUP_MAP_TABLE"
 #define APP_PFC_PRIORITY_TO_QUEUE_MAP_NAME          "MAP_PFC_PRIORITY_TO_QUEUE"
 
+/***** COUNTER DATABASE *****/
+
 #define COUNTERS_PORT_NAME_MAP          "COUNTERS_PORT_NAME_MAP"
 #define COUNTERS_TABLE                  "COUNTERS"
 #define COUNTERS_QUEUE_NAME_MAP         "COUNTERS_QUEUE_NAME_MAP"
@@ -50,10 +57,18 @@ namespace swss {
 #define DAEMON_TABLE_NAME "DAEMON_TABLE"
 #define DAEMON_LOGLEVEL "LOGLEVEL"
 
-#define APP_PFC_WD_TABLE_NAME "PFC_WD_TABLE"
+/***** PFC WATCHDOG DATABASE *****/
+
 #define PFC_WD_STATE_TABLE "PFC_WD_STATE_TABLE"
 #define PFC_WD_PORT_COUNTER_ID_LIST "PORT_COUNTER_ID_LIST"
 #define PFC_WD_QUEUE_COUNTER_ID_LIST "QUEUE_COUNTER_ID_LIST"
+
+/***** STATE DATABASE *****/
+
+#define STATE_PORT_TABLE_NAME           "PORT_TABLE"
+#define STATE_INTERFACE_TABLE_NAME      "INTERFACE_TABLE"
+
+/***** MISC *****/
 
 #define IPV4_NAME "IPv4"
 #define IPV6_NAME "IPv6"
