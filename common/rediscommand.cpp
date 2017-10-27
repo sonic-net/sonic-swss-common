b9d53c91d81413d5b19aa234f110d6520c21b331
lguohan <lguohan@gmail.com>
2017-10-27 11:55:08 -0700

    [rediscommand]: move class implementation to cpp file for rediscmd class (#152)
    * [rediscommand]: move class implementation to cpp file for rediscommand class
    
    * disable default copy constructor and default operator=

---
+++ common/rediscommand.cpp
@@ -0,0 +1,87 @@
+#include <vector>
+#include <hiredis/hiredis.h>
+#include "rediscommand.h"
+
+using namespace std;
+
+namespace swss {
+
+RedisCommand::RedisCommand()
+ : temp(NULL)
+{
+}
+
+RedisCommand::~RedisCommand()
+{
+    redisFreeCommand(temp);
+}
+
+void RedisCommand::format(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    int len = redisvFormatCommand(&temp, fmt, ap);
+    if (len == -1) {
+        throw std::bad_alloc();
+    } else if (len == -2) {
+        throw std::invalid_argument("fmt");
+    }
+}
+
+void RedisCommand::formatArgv(int argc, const char **argv, const size_t *argvlen)
+{
+    int len = redisFormatCommandArgv(&temp, argc, argv, argvlen);
+    if (len == -1) {
+        throw std::bad_alloc();
+    }
+}
+
+    /* Format HMSET key multiple field value command */
+void RedisCommand::formatHMSET(const std::string &key,
+                               const std::vector<FieldValueTuple> &values)
+{
+    if (values.empty()) throw std::invalid_argument("empty values");
+
+    const char* cmd = "HMSET";
+
+    std::vector<const char*> args = { cmd, key.c_str() };
+
+    for (const auto &fvt: values)
+    {
+        args.push_back(fvField(fvt).c_str());
+        args.push_back(fvValue(fvt).c_str());
+    }
+
+    formatArgv((int)args.size(), args.data(), NULL);
+}
+
+/* Format HSET key field value command */
+void RedisCommand::formatHSET(const std::string& key, const std::string& field,
+                              const std::string& value)
+{
+    format("HSET %s %s %s", key.c_str(), field.c_str(), value.c_str());
+}
+
+/* Format HGET key field command */
+void RedisCommand::formatHGET(const std::string& key, const std::string& field)
+{
+    format("HGET %s %s", key.c_str(), field.c_str());
+}
+
+/* Format HDEL key field command */
+void RedisCommand::formatHDEL(const std::string& key, const std::string& field)
+{
+    return format("HDEL %s %s", key.c_str(), field.c_str());
+}
+
+const char *RedisCommand::c_str() const
+{
+    return temp;
+}
+
+size_t RedisCommand::length() const
+{
+    return strlen(temp);
+}
+
+}
--- common/rediscommand.h
+++ common/rediscommand.h
@@ -15,77 +15,32 @@ typedef std::tuple<std::string, std::string, std::vector<FieldValueTuple> > KeyO
 
 class RedisCommand {
 public:
-    RedisCommand() : temp(NULL) { }
-    ~RedisCommand() { redisFreeCommand(temp); }
-    RedisCommand(RedisCommand& that);
-    RedisCommand& operator=(RedisCommand& that);
-    
-    void format(const char *fmt, ...) {
-        va_list ap;
-        va_start(ap, fmt);
-        int len = redisvFormatCommand(&temp, fmt, ap);
-        if (len == -1) {
-            throw std::bad_alloc();
-        } else if (len == -2) {
-            throw std::invalid_argument("fmt");
-        }
-    }
-    
-    void formatArgv(int argc, const char **argv, const size_t *argvlen) {
-        int len = redisFormatCommandArgv(&temp, argc, argv, argvlen);
-        if (len == -1) {
-            throw std::bad_alloc();
-        }
-    }
-    
+    RedisCommand();
+    ~RedisCommand();
+    RedisCommand(RedisCommand& that) = delete;
+    RedisCommand& operator=(RedisCommand& that) = delete;
+
+    void format(const char *fmt, ...);
+    void formatArgv(int argc, const char **argv, const size_t *argvlen);
+
     /* Format HMSET key multiple field value command */
     void formatHMSET(const std::string &key,
-                     const std::vector<FieldValueTuple> &values)
-    {
-        if (values.empty()) throw std::invalid_argument("empty values");
-
-        const char* cmd = "HMSET";
-
-        std::vector<const char*> args = { cmd, key.c_str() };
-
-        for (const auto &fvt: values)
-        {
-            args.push_back(fvField(fvt).c_str());
-            args.push_back(fvValue(fvt).c_str());
-        }
-        
-        formatArgv((int)args.size(), args.data(), NULL);
-    }
+                     const std::vector<FieldValueTuple> &values);
 
     /* Format HSET key field value command */
     void formatHSET(const std::string& key, const std::string& field,
-                    const std::string& value)
-    {
-        format("HSET %s %s %s", key.c_str(), field.c_str(), value.c_str());
-    }
+                    const std::string& value);
 
     /* Format HGET key field command */
-    void formatHGET(const std::string& key, const std::string& field)
-    {
-        format("HGET %s %s", key.c_str(), field.c_str());
-    }
+    void formatHGET(const std::string& key, const std::string& field);
 
     /* Format HDEL key field command */
-    void formatHDEL(const std::string& key, const std::string& field)
-    {
-        return format("HDEL %s %s", key.c_str(), field.c_str());
-    }
-    
-    const char *c_str() const
-    {
-        return temp;
-    }
-    
-    size_t length() const
-    {
-        return strlen(temp);
-    }
-    
+    void formatHDEL(const std::string& key, const std::string& field);
+
+    const char *c_str() const;
+
+    size_t length() const;
+
 private:
     char *temp;
 };
