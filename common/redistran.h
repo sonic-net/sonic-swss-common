b787d4bd0efc1d6959dfcc48909fb9089935e452
lgh <lgh@contoso.com>
2017-10-29 09:28:54 +0000

    move RedisSelect/RedisTransactioner implementation to cpp files

--- common/redistran.h
+++ common/redistran.h
@@ -10,75 +10,20 @@ namespace swss {
 
 class RedisTransactioner {
 public:
-    RedisTransactioner(DBConnector *db) : m_db(db) { }
+    RedisTransactioner(DBConnector *db);
 
-    ~RedisTransactioner() { clearResults(); }
+    ~RedisTransactioner();
 
     /* Start a transaction */
-    void multi()
-    {
-        m_expectedResults.clear();
-        RedisReply r(m_db, "MULTI", REDIS_REPLY_STATUS);
-        r.checkStatusOK();
-    }
+    void multi();
 
     /* Execute a transaction and get results */
-    bool exec()
-    {
-        using namespace std;
-
-        RedisReply r(m_db, "EXEC");
-        redisReply *reply = r.getContext();
-        size_t size = reply->elements;
-
-        // if meet error in transaction
-        if (reply->type != REDIS_REPLY_ARRAY)
-        {
-            return false;
-        }
-
-        if (size != m_expectedResults.size())
-            throw system_error(make_error_code(errc::io_error),
-                               "Got to different nuber of answers!");
-
-        clearResults();
-        for (unsigned int i = 0; i < size; i++)
-        {
-            int expectedType = m_expectedResults.front();
-            m_expectedResults.pop_front();
-            if (expectedType != reply->element[i]->type)
-            {
-                SWSS_LOG_ERROR("Expected to get redis type %d got type %d",
-                              expectedType, reply->element[i]->type);
-                throw system_error(make_error_code(errc::io_error),
-                                   "Got unexpected result");
-            }
-        }
-
-        for (size_t i = 0; i < size; i++)
-            m_results.push_back(reply->element[i]);
-
-        /* Free only the array memory */
-        r.release();
-        free(reply->element);
-        free(reply);
-        return true;
-    }
+    bool exec();
 
     /* Send a command within a transaction */
-    void enqueue(std::string command, int expectedType)
-    {
-        RedisReply r(m_db, command, REDIS_REPLY_STATUS);
-        r.checkStatusQueued();
-        m_expectedResults.push_back(expectedType);
-    }
+    void enqueue(std::string command, int expectedType);
 
-    redisReply *dequeueReply()
-    {
-        redisReply *ret = m_results.front();
-        m_results.pop_front();
-        return ret;
-    }
+    redisReply *dequeueReply();
 
 protected:
     DBConnector *m_db;
@@ -87,15 +32,7 @@ private:
     std::deque<int> m_expectedResults;
     std::deque<redisReply *> m_results;
 
-    void clearResults()
-    {
-        if (m_results.empty()) return;
-        for (auto r: m_results)
-        {
-            freeReplyObject(r);
-        }
-        m_results.clear();
-    }
+    void clearResults();
 };
 
 }
