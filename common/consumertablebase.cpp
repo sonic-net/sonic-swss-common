fe6bdec2ca7e9538fc73314798e9bcae13ce579c
Guohan Lu <gulv@microsoft.com>
2017-10-30 19:11:32 +0000

    remove extra kco copy

--- common/consumertablebase.cpp
+++ common/consumertablebase.cpp
@@ -15,28 +15,11 @@ ConsumerTableBase::~ConsumerTableBase()
 
 void ConsumerTableBase::pop(KeyOpFieldsValuesTuple &kco, std::string prefix)
 {
-    if (m_buffer.empty())
-    {
-        pops(m_buffer, prefix);
-
-        if (m_buffer.empty())
-        {
-            auto& values = kfvFieldsValues(kco);
-            values.clear();
-            kfvKey(kco).clear();
-            kfvOp(kco).clear();
-            return;
-        }
-    }
-
-    kco = m_buffer.front();
-    m_buffer.pop_front();
+    pop(kfvKey(kco), kfvOp(kco), kfvFieldsValues(kco), prefix);
 }
 
 void ConsumerTableBase::pop(std::string &key, std::string &op, std::vector<FieldValueTuple> &fvs, std::string prefix)
 {
-    KeyOpFieldsValuesTuple kco;
-
     if (m_buffer.empty())
     {
         pops(m_buffer, prefix);
@@ -50,12 +33,13 @@ void ConsumerTableBase::pop(std::string &key, std::string &op, std::vector<Field
         }
     }
 
-    kco = m_buffer.front();
-    m_buffer.pop_front();
+    KeyOpFieldsValuesTuple &kco = m_buffer.front();
 
     key = kfvKey(kco);
     op = kfvOp(kco);
     fvs = kfvFieldsValues(kco);
+
+    m_buffer.pop_front();
 }
 
 }
