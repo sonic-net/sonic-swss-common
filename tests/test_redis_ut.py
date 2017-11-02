adf7d4cb3c22061bf781ef6ce7b79964ce505730
lguohan <lguohan@gmail.com>
2017-11-02 15:43:30 -0700

    add support for SubscriberStateTable in python library (#157)

--- tests/test_redis_ut.py
+++ tests/test_redis_ut.py
@@ -12,3 +12,20 @@ def test_ProducerStateTable():
     assert op == "SET"
     assert len(cfvs) == 1
     assert cfvs[0] == ('a', 'b')
+
+def test_SubscriberStateTable():
+    db = swsscommon.DBConnector(0, "localhost", 6379, 0)
+    t = swsscommon.Table(db, "testsst", '|')
+    sel = swsscommon.Select()
+    cst = swsscommon.SubscriberStateTable(db, "testsst")
+    sel.addSelectable(cst)
+    fvs = swsscommon.FieldValuePairs([('a','b')])
+    t.set("aaa", fvs)
+    (state, c, fd) = sel.select()
+    assert state == swsscommon.Select.OBJECT
+    cfvs = swsscommon.FieldValuePairs([])
+    (key, op) = cst.pop(cfvs)
+    assert key == "aaa"
+    assert op == "SET"
+    assert len(cfvs) == 1
+    assert cfvs[0] == ('a', 'b')
