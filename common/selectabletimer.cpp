a67ee684ed33083f61df83cd1ba9c047e5ed8b68
Qi Luo <qiluo-msft@users.noreply.github.com>
2017-11-15 00:15:18 -0800

    Add SelectableTimer class (#160)
    * Add SelectableTimer class
    
    Signed-off-by: Qi Luo <qiluo-msft@users.noreply.github.com>
    
    * SelectableTimer adds start/stop/setInterval interfaces
    
    Signed-off-by: Qi Luo <qiluo-msft@users.noreply.github.com>
    
    * Refine with SWSS_LOG_THROW
    
    Signed-off-by: Qi Luo <qiluo-msft@users.noreply.github.com>
    
    * SelectableTimer adds reset interface
    
    Signed-off-by: Qi Luo <qiluo-msft@users.noreply.github.com>

---
+++ common/selectabletimer.cpp
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <iostream>
+#include <system_error>
+
+#include "common/logger.h"
+#include "common/selectabletimer.h"
+
+namespace swss {
+
+SelectableTimer::SelectableTimer(const timespec& interval)
+    : m_zero({{0, 0}, {0, 0}})
+{
+    // Create the timer
+    m_tfd = timerfd_create(CLOCK_REALTIME, 0);
+    if (m_tfd == -1)
+    {
+        SWSS_LOG_THROW("failed to create timerfd, errno: %s", strerror(errno));
+    }
+    setInterval(interval);
+}
+
+SelectableTimer::~SelectableTimer()
+{
+    int err;
+
+    do
+    {
+        err = close(m_tfd);
+    }
+    while(err == -1 && errno == EINTR);
+}
+
+void SelectableTimer::start()
+{
+    // Set the timer interval and the timer is automatically started
+    int rc = timerfd_settime(m_tfd, 0, &m_interval, NULL);
+    if (rc == -1)
+    {
+        SWSS_LOG_THROW("failed to set timerfd, errno: %s", strerror(errno));
+    }
+}
+
+void SelectableTimer::stop()
+{
+    // Set the timer interval and the timer is automatically started
+    int rc = timerfd_settime(m_tfd, 0, &m_zero, NULL);
+    if (rc == -1)
+    {
+        SWSS_LOG_THROW("failed to set timerfd to zero, errno: %s", strerror(errno));
+    }
+}
+
+void SelectableTimer::reset()
+{
+    stop();
+    start();
+}
+
+void SelectableTimer::setInterval(const timespec& interval)
+{
+    // The initial expiration and intervals to caller specified
+    m_interval.it_value = interval;
+    m_interval.it_interval = interval;
+}
+
+void SelectableTimer::addFd(fd_set *fd)
+{
+    FD_SET(m_tfd, fd);
+}
+
+int SelectableTimer::readCache()
+{
+    return Selectable::NODATA;
+}
+
+void SelectableTimer::readMe()
+{
+    uint64_t r;
+
+    ssize_t s;
+    do
+    {
+        // Read the timefd so it will be reset
+        s = read(m_tfd, &r, sizeof(uint64_t));
+    }
+    while(s == -1 && errno == EINTR);
+
+    if (s != sizeof(uint64_t))
+    {
+        SWSS_LOG_THROW("read failed, errno: %s", strerror(errno));
+    }
+}
+
+bool SelectableTimer::isMe(fd_set *fd)
+{
+    return FD_ISSET(m_tfd, fd);
+}
+
+}
---
+++ common/selectabletimer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <limits>
+#include <sys/timerfd.h>
+#include "selectable.h"
+
+namespace swss {
+
+class SelectableTimer : public Selectable
+{
+public:
+    SelectableTimer(const timespec& interval);
+    virtual ~SelectableTimer();
+    void start();
+    void stop();
+    void reset();
+    void setInterval(const timespec& interval);
+
+    virtual void addFd(fd_set *fd);
+    virtual bool isMe(fd_set *fd);
+    virtual int readCache();
+    virtual void readMe();
+
+private:
+    int m_tfd;
+    itimerspec m_interval;
+    itimerspec m_zero;
+};
+
+}
