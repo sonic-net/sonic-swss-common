53df32e07fff5d6ee39fbfa7adba8029da6a719c
Rodny Molina <rodnymolina@hotmail.com>
2018-02-15 06:21:27 -0800

    Extending IpAddress class with method to return the scope of any given ip-address (#187)
    * Extending IpAddress class with method to return the scope of any given ip address
    
    This code is required by link-local functionality (sonic-swss PR/437).
    
    UTs execution:
    
    rmolina@6175c07b92ba:/sonic/src/sonic-swss-common$ sudo ./tests/tests
    ...
    [----------] 2 tests from IpAddresses
    [ RUN      ] IpAddresses.empty
    [       OK ] IpAddresses.empty (0 ms)
    [ RUN      ] IpAddresses.contains
    [       OK ] IpAddresses.contains (1 ms)
    [----------] 2 tests from IpAddresses (1 ms total)
    
    [----------] 1 test from IpAddress
    [ RUN      ] IpAddress.getAddrScope
    [       OK ] IpAddress.getAddrScope (0 ms)
    [----------] 1 test from IpAddress (0 ms total)
    ...
    
    * Reducing scope of auxiliar variables.

--- tests/ipaddress_ut.cpp
+++ tests/ipaddress_ut.cpp
@@ -33,3 +33,42 @@ TEST(IpAddresses, contains)
     EXPECT_TRUE(ips.contains(ips_1));
     EXPECT_FALSE(ips.contains(ips_2));
 }
+
+TEST(IpAddress, getAddrScope)
+{
+    // IPv4 prefixes
+    IpAddress ip1("0.0.0.0");
+    IpAddress ip2("169.1.1.1");
+    IpAddress ip3("169.254.0.1");
+    IpAddress ip4("169.254.255.255");
+    IpAddress ip5("169.253.1.1");
+    IpAddress ip6("169.255.1.1");
+    IpAddress ip7("127.0.0.1");
+
+    // IPv6 prefixes
+    IpAddress ip11("2001:4898:f0:f153:357c:77b2:49c9:627c");
+    IpAddress ip12("fe80::1");
+    IpAddress ip13("fe80:1:1:1:1:1:1:1");
+    IpAddress ip14("fe99::1");
+    IpAddress ip15("feb0:1::1");
+    IpAddress ip16("febf:1::1");
+    IpAddress ip17("fec0:1::1");
+    IpAddress ip18("::1");
+
+    EXPECT_EQ(IpAddress::AddrScope::GLOBAL_SCOPE, ip1.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::GLOBAL_SCOPE, ip2.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::LINK_SCOPE,   ip3.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::LINK_SCOPE,   ip4.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::GLOBAL_SCOPE, ip5.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::GLOBAL_SCOPE, ip6.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::HOST_SCOPE,   ip7.getAddrScope());
+
+    EXPECT_EQ(IpAddress::AddrScope::GLOBAL_SCOPE, ip11.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::LINK_SCOPE,   ip12.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::LINK_SCOPE,   ip13.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::LINK_SCOPE,   ip14.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::LINK_SCOPE,   ip15.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::LINK_SCOPE,   ip16.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::GLOBAL_SCOPE, ip17.getAddrScope());
+    EXPECT_EQ(IpAddress::AddrScope::HOST_SCOPE,   ip18.getAddrScope());
+}
