5dcd3f082ac9951d6d1ca58048f0a2151e638ccb
Qi Luo <qiluo-msft@users.noreply.github.com>
2018-05-23 23:14:04 +0000

    Improve ConsumerStateTable: del after consumed
    Signed-off-by: Qi Luo <qiluo-msft@users.noreply.github.com>

--- common/consumer_state_table_pops.lua
+++ common/consumer_state_table_pops.lua
@@ -1,3 +1,4 @@
+redis.replicate_commands()
 local ret = {}
 local keys = redis.call('SPOP', KEYS[1], ARGV[1])
 local n = table.getn(keys)
@@ -5,5 +6,6 @@ for i = 1, n do
    local key = keys[i]
    local values = redis.call('HGETALL', KEYS[2] .. key)
    table.insert(ret, {key, values})
+   redis.call('DEL', KEYS[2] .. key)
 end
 return ret
