ffa48d09dd571fc1a900372131be6788caa65208
Qi Luo <qiluo-msft@users.noreply.github.com>
2018-08-31 17:52:31 -0700

    Add new function NotificationConsumer::pops, add unit test (#226)
    * Add new function NotificationConsumer::pops, add unit test
    * Improve NotificationConsumer::pops: continue reading redis socket if data still ready
    * Set a pop size to prevent DoS attach

--- tests/ntf_ut.cpp
+++ tests/ntf_ut.cpp
@@ -11,21 +11,19 @@
 #include "common/selectableevent.h"
 #include "common/select.h"
 #include "common/logger.h"
+#include "common/table.h"
 
 std::shared_ptr<std::thread> notification_thread;
 
-volatile int messages = 5000;
+const int messages = 5000;
 
-void ntf_thread()
+void ntf_thread(swss::NotificationConsumer& nc)
 {
     SWSS_LOG_ENTER();
 
-    swss::DBConnector g_dbNtf(ASIC_DB, "localhost", 6379, 0);
-    swss::NotificationConsumer g_redisNotifications(&g_dbNtf, "NOTIFICATIONS");
-
     swss::Select s;
 
-    s.addSelectable(&g_redisNotifications);
+    s.addSelectable(&nc);
 
     int collected = 0;
 
@@ -43,24 +41,27 @@ void ntf_thread()
             std::string data;
             std::vector<swss::FieldValueTuple> values;
 
-            g_redisNotifications.pop(op, data, values);
+            nc.pop(op, data, values);
 
             SWSS_LOG_INFO("notification: op = %s, data = %s", op.c_str(), data.c_str());
+            EXPECT_EQ(op, "ntf");
+            int i = stoi(data);
+            EXPECT_EQ(i, collected + 1);
 
             collected++;
         }
     }
+    EXPECT_EQ(collected, messages);
 }
 
 TEST(Notifications, test)
 {
     SWSS_LOG_ENTER();
 
-    notification_thread = std::make_shared<std::thread>(std::thread(ntf_thread));
-
-    sleep(1); // give time to subscribe to not miss notification
-
     swss::DBConnector dbNtf(ASIC_DB, "localhost", 6379, 0);
+    swss::NotificationConsumer nc(&dbNtf, "NOTIFICATIONS");
+    notification_thread = std::make_shared<std::thread>(std::thread(ntf_thread, std::ref(nc)));
+
     swss::NotificationProducer notifications(&dbNtf, "NOTIFICATIONS");
 
     std::vector<swss::FieldValueTuple> entry;
@@ -74,4 +75,46 @@ TEST(Notifications, test)
 
     notification_thread->join();
 }
-//FIXME: no tests inside
+
+TEST(Notifications, pops)
+{
+    SWSS_LOG_ENTER();
+
+    swss::DBConnector dbNtf(ASIC_DB, "localhost", 6379, 0);
+    swss::NotificationConsumer nc(&dbNtf, "NOTIFICATIONS", 100, (size_t)messages);
+    swss::NotificationProducer notifications(&dbNtf, "NOTIFICATIONS");
+
+    std::vector<swss::FieldValueTuple> entry;
+    for(int i = 0; i < messages; i++)
+    {
+        auto s = std::to_string(i+1);
+        notifications.send("ntf", s, entry);
+    }
+
+    // Pop all the notifications
+    swss::Select s;
+    s.addSelectable(&nc);
+    swss::Selectable *sel;
+
+    int result = s.select(&sel);
+
+    EXPECT_EQ(result, swss::Select::OBJECT);
+
+    std::deque<swss::KeyOpFieldsValuesTuple> vkco;
+    nc.pops(vkco);
+    EXPECT_EQ(vkco.size(), (size_t)messages);
+
+    for (size_t collected = 0; collected < vkco.size(); collected++)
+    {
+        auto data = kfvKey(vkco[collected]);
+        auto op = kfvOp(vkco[collected]);
+
+        EXPECT_EQ(op, "ntf");
+        int i = stoi(data);
+        EXPECT_EQ((size_t)i, collected + 1);
+    }
+
+    // Pop again and get nothing
+    nc.pops(vkco);
+    EXPECT_TRUE(vkco.empty());
+}
