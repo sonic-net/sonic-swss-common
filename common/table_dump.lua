28e49b9dda3df823a0725bf10c59c9dcf783e408
Taoyu Li <taoyl@microsoft.com>
2018-11-14 20:08:40 +0000

    Improve table.dump() performance

--- common/table_dump.lua
+++ common/table_dump.lua
@@ -4,10 +4,11 @@ local res = {}
 for i,k in pairs(keys) do
 
    local skeys = redis.call("HKEYS", k)
-   local sres={}
 
-   for j,sk in pairs(skeys) do
-       sres[sk] = redis.call("HGET", k, sk)
+   local sres={}
+   local flat_map = redis.call('HGETALL', k)
+   for j = 1, #flat_map, 2 do
+       sres[flat_map[j]] = flat_map[j + 1]
    end
 
    res[k] = sres
