0e1b98d2daa63d21fe75da3bf912db9eb860c87b
Pavel Shirshov <pavelsh@microsoft.com>
2019-03-06 14:42:47 -0800

    Change wording of the comment. Fix typos.

--- common/selectable.h
+++ common/selectable.h
@@ -23,11 +23,14 @@ public:
     /* Read all data from the fd assicaited with Selectable */
     virtual void readData() = 0;
 
-    /* true if Selectable has data in it for immediate read
-       this is supposed to be implemented in Selectibles, which implements hasCachedData().
-       it's possible to have a case, when an application read all cached data after 1 signal,
-       and we have stale selectible with no data in the m_ready queue in Select class.
-       The class without hasCachedData never is going to be in m_ready state without the data */
+    /*
+       true if Selectable has data in it for immediate read
+       This method is intended to be implemented in subclasses of Selectable
+       which also implement the hasCachedData() method
+       it's possible to have a case, when an application read all cached data after a signal,
+       and we have a Selectable with no data in the m_ready queue (Select class).
+       The class without hasCachedData never is going to be in m_ready state without the data
+    */
     virtual bool hasData()
     {
         return true;
