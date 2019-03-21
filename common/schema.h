e58f0282e6816e0c8237a79c8bc2cd0b1afb7fb3
Pterosaur <ganze718@gmail.com>
2019-03-21 13:24:06 +0800

    Add STATE_VXLAN_TABLE_NAME macro to schema
    Signed-off-by: Ze Gan zegan@microsoft.com

--- common/schema.h
+++ common/schema.h
@@ -185,6 +185,7 @@ namespace swss {
 #define STATE_VRF_TABLE_NAME                        "VRF_TABLE"
 #define STATE_MGMT_PORT_TABLE_NAME                  "MGMT_PORT_TABLE"
 #define STATE_NEIGH_RESTORE_TABLE_NAME              "NEIGH_RESTORE_TABLE"
+#define STATE_VXLAN_TABLE_NAME                      "VXLAN_TABLE"
 
 /***** MISC *****/
 
