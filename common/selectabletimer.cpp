e5bc3b84096e019d98f09cfb622cc42cb4f16b04
Renuka Manavalan <remanava@microsoft.com>
2019-07-09 20:58:10 +0000

    Fix formatter.

--- common/selectabletimer.cpp
+++ common/selectabletimer.cpp
@@ -108,7 +108,7 @@ void SelectableTimer::readData()
             "Failed to read timerfd. s=%ld", s)
 
     if (s != sizeof(uint64_t)) {
-        SWSS_LOG_ERROR("Benign failure to read from timerfd return=%d error=%s",
+        SWSS_LOG_ERROR("Benign failure to read from timerfd return=%zd error=%s",
                 s, strerror(errno));
     }
 
