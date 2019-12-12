0f9198d1077bbefb6cfb0f24e0722ba5a1e7a2fd
dzhang <d.zhang@alibaba-inc.com>
2019-12-12 13:40:41 -0800

    update code format

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,11 +2,11 @@
 
 int main(int argc, char* argv[])
 {
-  testing::InitGoogleTest(&argc, argv);
-  //run multiDB test first, which init the db config
-  ::testing::GTEST_FLAG(filter) = "DBConnector.multi_db_test";
-  EXPECT_EQ(RUN_ALL_TESTS(), 0);
-  //run all other test cases
-  ::testing::GTEST_FLAG(filter) = "*.*-DBConnector.multi_db_test";
-  return RUN_ALL_TESTS();
+    testing::InitGoogleTest(&argc, argv);
+    //run multiDB test first, which init the db config
+    ::testing::GTEST_FLAG(filter) = "DBConnector.multi_db_test";
+    EXPECT_EQ(RUN_ALL_TESTS(), 0);
+    //run all other test cases
+    ::testing::GTEST_FLAG(filter) = "*.*-DBConnector.multi_db_test";
+    return RUN_ALL_TESTS();
 }
