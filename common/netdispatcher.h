7ee11792342f2673ea6905952d556e3f9a27234d
Kamil Cudnik <kcudnik@gmail.com>
2020-01-15 09:13:19 +0100

    Add unregister method to NetDispatcher (#326)

--- common/netdispatcher.h
+++ common/netdispatcher.h
@@ -8,6 +8,7 @@
 #include <netlink/route/rtnl.h>
 
 #include <map>
+#include <mutex>
 
 #include "netmsg.h"
 
@@ -15,13 +16,15 @@ namespace swss {
 
 class NetDispatcher {
 public:
-    /**/
+
+    /*
+     * Get singlton instance*/
     static NetDispatcher& getInstance();
 
     /*
      * Register callback class according to message-type.
      *
-     * Throw exception if
+     * Throw exception if callback is already registered.
      */
     void registerMessageHandler(int nlmsg_type, NetMsg *callback);
 
@@ -30,13 +33,32 @@ public:
      */
     void onNetlinkMessage(struct nl_msg *msg);
 
+    /*
+     * Unregister callback according to message-type.
+     *
+     * Throw exception if callback is not registered.
+     */
+    void unregisterMessageHandler(int nlmsg_type);
+
 private:
+
     NetDispatcher() = default;
+
+    NetDispatcher(const NetDispatcher&) = delete;
+
+    NetMsg* getCallback(int nlmsg_type);
+
+private:
 
     /* nl_msg_parse callback API */
     static void nlCallback(struct nl_object *obj, void *context);
 
-    std::map<int, NetMsg * > m_handlers;
+    std::map<int, NetMsg*> m_handlers;
+
+    /*
+     * Mutex protecting register, unregister and get callback methods.
+     */
+    std::mutex m_mutex;
 };
 
 }
