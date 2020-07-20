d79bcd3f25f3bc2d5f6ec0a8d4944c19961ecbad
Abhishek Dosi <abdosi@microsoft.com>
2020-07-20 09:09:43 -0700

    Added Support to get DB Id and DB Namespace from Selectable Object. It is helpful in application that are doing select and need to the event is generated from which DB and namespace so that any parituclar action can be taken if needed.
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>

--- common/redisselect.h
+++ common/redisselect.h
@@ -20,6 +20,8 @@ public:
     bool hasCachedData() override;
     bool initializedWithData() override;
     void updateAfterRead() override;
+    int getDbId() override;
+    std::string getDbNamespace() override;
 
     /* Create a new redisContext, SELECT DB and SUBSCRIBE */
     void subscribe(DBConnector* db, const std::string &channelName);
