074ad7bb06d4d1768095059dc853df1415f8d49f
Judy Joseph <judy.joseph@microsoft.com>
2020-08-02 19:52:46 -0700

    Adding back PR#364

--- common/dbconnector.h
+++ common/dbconnector.h
@@ -7,10 +7,19 @@
 #include <utility>
 
 #include <hiredis/hiredis.h>
+#define EMPTY_NAMESPACE std::string()
 
 namespace swss {
 
 class DBConnector;
+
+class RedisInstInfo
+{
+public:
+    std::string unixSocketPath;
+    std::string hostname;
+    int port;
+};
 
 class SonicDBInfo
 {
@@ -23,26 +32,35 @@ public:
 class SonicDBConfig
 {
 public:
-    static void initialize(const std::string &file = DEFAULT_SONIC_DB_CONFIG_FILE);
-    static std::string getDbInst(const std::string &dbName);
-    static int getDbId(const std::string &dbName);
-    static std::string getSeparator(const std::string &dbName);
-    static std::string getSeparator(int dbId);
+    static void initialize(const std::string &file = DEFAULT_SONIC_DB_CONFIG_FILE, const std::string &nameSpace = EMPTY_NAMESPACE);
+    static void initializeGlobalConfig(const std::string &file = DEFAULT_SONIC_DB_GLOBAL_CONFIG_FILE);
+    static std::string getDbInst(const std::string &dbName, const std::string &nameSpace = EMPTY_NAMESPACE);
+    static int getDbId(const std::string &dbName, const std::string &nameSpace = EMPTY_NAMESPACE);
+    static std::string getSeparator(const std::string &dbName, const std::string &nameSpace = EMPTY_NAMESPACE);
+    static std::string getSeparator(int dbId, const std::string &nameSpace = EMPTY_NAMESPACE);
     static std::string getSeparator(const DBConnector* db);
-    static std::string getDbSock(const std::string &dbName);
-    static std::string getDbHostname(const std::string &dbName);
-    static int getDbPort(const std::string &dbName);
+    static std::string getDbSock(const std::string &dbName, const std::string &nameSpace = EMPTY_NAMESPACE);
+    static std::string getDbHostname(const std::string &dbName, const std::string &nameSpace = EMPTY_NAMESPACE);
+    static int getDbPort(const std::string &dbName, const std::string &nameSpace = EMPTY_NAMESPACE);
+    static std::vector<std::string> getNamespaces();
     static bool isInit() { return m_init; };
+    static bool isGlobalInit() { return m_global_init; };
 
 private:
     static constexpr const char *DEFAULT_SONIC_DB_CONFIG_FILE = "/var/run/redis/sonic-db/database_config.json";
-    // { instName, { unix_socket_path, {hostname, port} } }
-    static std::unordered_map<std::string, std::pair<std::string, std::pair<std::string, int>>> m_inst_info;
-    // { dbName, {instName, dbId} }
-    static std::unordered_map<std::string, SonicDBInfo> m_db_info;
-    // { dbIp, separator }
-    static std::unordered_map<int, std::string> m_db_separator;
+    static constexpr const char *DEFAULT_SONIC_DB_GLOBAL_CONFIG_FILE = "/var/run/redis/sonic-db/database_global.json";
+    // { namespace { instName, { unix_socket_path, hostname, port } } }
+    static std::unordered_map<std::string, std::unordered_map<std::string, RedisInstInfo>> m_inst_info;
+    // { namespace, { dbName, {instName, dbId, separator} } }
+    static std::unordered_map<std::string, std::unordered_map<std::string, SonicDBInfo>> m_db_info;
+    // { namespace, { dbId, separator } }
+    static std::unordered_map<std::string, std::unordered_map<int, std::string>> m_db_separator;
     static bool m_init;
+    static bool m_global_init;
+    static void parseDatabaseConfig(const std::string &file,
+                                    std::unordered_map<std::string, RedisInstInfo> &inst_entry,
+                                    std::unordered_map<std::string, SonicDBInfo> &db_entry,
+                                    std::unordered_map<int, std::string> &separator_entry);
 };
 
 class DBConnector
@@ -59,13 +77,14 @@ public:
      */
     DBConnector(int dbId, const std::string &hostname, int port, unsigned int timeout);
     DBConnector(int dbId, const std::string &unixPath, unsigned int timeout);
-    DBConnector(const std::string &dbName, unsigned int timeout, bool isTcpConn = false);
+    DBConnector(const std::string &dbName, unsigned int timeout, bool isTcpConn = false, const std::string &nameSpace = EMPTY_NAMESPACE);
 
     ~DBConnector();
 
     redisContext *getContext() const;
     int getDbId() const;
     std::string getDbName() const;
+    std::string getNamespace() const;
 
     static void select(DBConnector *db);
 
@@ -84,6 +103,7 @@ private:
     redisContext *m_conn;
     int m_dbId;
     std::string m_dbName;
+    std::string m_namespace;
 };
 
 }
--- tests/main.cpp
+++ tests/main.cpp
@@ -7,6 +7,8 @@ using namespace swss;
 
 string existing_file = "./tests/redis_multi_db_ut_config/database_config.json";
 string nonexisting_file = "./tests/redis_multi_db_ut_config/database_config_nonexisting.json";
+string global_existing_file = "./tests/redis_multi_db_ut_config/database_global.json";
+string global_nonexisting_file = "./tests/redis_multi_db_ut_config/database_global_nonexisting.json";
 
 class SwsscommonEnvironment : public ::testing::Environment {
 public:
@@ -32,6 +34,28 @@ public:
         SonicDBConfig::initialize(existing_file);
         cout<<"INIT: load local db config file, isInit = "<<SonicDBConfig::isInit()<<endl;
         EXPECT_TRUE(SonicDBConfig::isInit());
+
+        // Test the database_global.json file
+        // by default , global_init should be false
+        cout<<"Default : isGlobalInit = "<<SonicDBConfig::isGlobalInit()<<endl;
+        EXPECT_FALSE(SonicDBConfig::isGlobalInit());
+
+        // load nonexisting file, should throw exception with NO file existing
+        try
+        {
+            cout<<"INIT: loading nonexisting global db config file"<<endl;
+            SonicDBConfig::initializeGlobalConfig(global_nonexisting_file);
+        }
+        catch (exception &e)
+        {
+            EXPECT_TRUE(strstr(e.what(), "Sonic database global config file doesn't exist"));
+        }
+        EXPECT_FALSE(SonicDBConfig::isGlobalInit());
+
+        // load local global file, init should be true
+        SonicDBConfig::initializeGlobalConfig(global_existing_file);
+        cout<<"INIT: load global db config file, isInit = "<<SonicDBConfig::isGlobalInit()<<endl;
+        EXPECT_TRUE(SonicDBConfig::isGlobalInit());
     }
 };
 
