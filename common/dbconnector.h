48096dbadf8b6760289fa9dc2b5f07c50b371c8f
judyjoseph <53951155+judyjoseph@users.noreply.github.com>
2020-08-12 11:17:55 -0700

    Revert "DBconnector to support namespaces. (#371)" (#375)
    This reverts commit 1943e2b754e4f045867fe5d480b75a7e2c173d9e.

--- common/dbconnector.h
+++ common/dbconnector.h
@@ -7,19 +7,10 @@
 #include <utility>
 
 #include <hiredis/hiredis.h>
-#define EMPTY_NAMESPACE std::string()
 
 namespace swss {
 
 class DBConnector;
-
-class RedisInstInfo
-{
-public:
-    std::string unixSocketPath;
-    std::string hostname;
-    int port;
-};
 
 class SonicDBInfo
 {
@@ -33,35 +24,25 @@ class SonicDBConfig
 {
 public:
     static void initialize(const std::string &file = DEFAULT_SONIC_DB_CONFIG_FILE);
-    static void initializeGlobalConfig(const std::string &file = DEFAULT_SONIC_DB_GLOBAL_CONFIG_FILE);
-    static void validateNamespace(const std::string &nameSpace);
-    static std::string getDbInst(const std::string &dbName, const std::string &nameSpace = EMPTY_NAMESPACE);
-    static int getDbId(const std::string &dbName, const std::string &nameSpace = EMPTY_NAMESPACE);
-    static std::string getSeparator(const std::string &dbName, const std::string &nameSpace = EMPTY_NAMESPACE);
-    static std::string getSeparator(int dbId, const std::string &nameSpace = EMPTY_NAMESPACE);
+    static std::string getDbInst(const std::string &dbName);
+    static int getDbId(const std::string &dbName);
+    static std::string getSeparator(const std::string &dbName);
+    static std::string getSeparator(int dbId);
     static std::string getSeparator(const DBConnector* db);
-    static std::string getDbSock(const std::string &dbName, const std::string &nameSpace = EMPTY_NAMESPACE);
-    static std::string getDbHostname(const std::string &dbName, const std::string &nameSpace = EMPTY_NAMESPACE);
-    static int getDbPort(const std::string &dbName, const std::string &nameSpace = EMPTY_NAMESPACE);
-    static std::vector<std::string> getNamespaces();
+    static std::string getDbSock(const std::string &dbName);
+    static std::string getDbHostname(const std::string &dbName);
+    static int getDbPort(const std::string &dbName);
     static bool isInit() { return m_init; };
-    static bool isGlobalInit() { return m_global_init; };
 
 private:
     static constexpr const char *DEFAULT_SONIC_DB_CONFIG_FILE = "/var/run/redis/sonic-db/database_config.json";
-    static constexpr const char *DEFAULT_SONIC_DB_GLOBAL_CONFIG_FILE = "/var/run/redis/sonic-db/database_global.json";
-    // { namespace { instName, { unix_socket_path, hostname, port } } }
-    static std::unordered_map<std::string, std::unordered_map<std::string, RedisInstInfo>> m_inst_info;
-    // { namespace, { dbName, {instName, dbId, separator} } }
-    static std::unordered_map<std::string, std::unordered_map<std::string, SonicDBInfo>> m_db_info;
-    // { namespace, { dbId, separator } }
-    static std::unordered_map<std::string, std::unordered_map<int, std::string>> m_db_separator;
+    // { instName, { unix_socket_path, {hostname, port} } }
+    static std::unordered_map<std::string, std::pair<std::string, std::pair<std::string, int>>> m_inst_info;
+    // { dbName, {instName, dbId} }
+    static std::unordered_map<std::string, SonicDBInfo> m_db_info;
+    // { dbIp, separator }
+    static std::unordered_map<int, std::string> m_db_separator;
     static bool m_init;
-    static bool m_global_init;
-    static void parseDatabaseConfig(const std::string &file,
-                                    std::unordered_map<std::string, RedisInstInfo> &inst_entry,
-                                    std::unordered_map<std::string, SonicDBInfo> &db_entry,
-                                    std::unordered_map<int, std::string> &separator_entry);
 };
 
 class DBConnector
@@ -79,14 +60,12 @@ public:
     DBConnector(int dbId, const std::string &hostname, int port, unsigned int timeout);
     DBConnector(int dbId, const std::string &unixPath, unsigned int timeout);
     DBConnector(const std::string &dbName, unsigned int timeout, bool isTcpConn = false);
-    DBConnector(const std::string &dbName, unsigned int timeout, bool isTcpConn, const std::string &nameSpace);
 
     ~DBConnector();
 
     redisContext *getContext() const;
     int getDbId() const;
     std::string getDbName() const;
-    std::string getNamespace() const;
 
     static void select(DBConnector *db);
 
@@ -105,7 +84,6 @@ private:
     redisContext *m_conn;
     int m_dbId;
     std::string m_dbName;
-    std::string m_namespace;
 };
 
 }
