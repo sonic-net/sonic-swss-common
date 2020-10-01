bb5f793369153b7bf9b6fe380a8855357055e201
Nikhil Kelapure <nikhil.kelapure@broadcom.com>
2020-10-01 02:58:55 -0700

    [warm reboot] Warm Reboot Support for EVPN VXLAN
    Addressed review comments

--- common/warm_restart.cpp
+++ common/warm_restart.cpp
@@ -10,7 +10,10 @@ const WarmStart::WarmStartStateNameMap WarmStart::warmStartStateNameMap =
 {
     {INITIALIZED,   "initialized"},
     {RESTORED,      "restored"},
-    {RECONCILED,    "reconciled"}
+    {REPLAYED,      "replayed"},
+    {RECONCILED,    "reconciled"},
+    {WSDISABLED,    "disabled"},
+    {WSUNKNOWN,     "unknown"}
 };
 
 const WarmStart::DataCheckStateNameMap WarmStart::dataCheckStateNameMap =
@@ -183,7 +186,6 @@ bool WarmStart::isSystemWarmRebootEnabled(void)
 
 void WarmStart::getWarmStartState(const std::string &app_name, WarmStartState &state)
 {
-    //WarmStartStateNameMap::iterator it;
     std::string statestr;
 
     auto& warmStart = getInstance();
@@ -200,21 +202,20 @@ void WarmStart::getWarmStartState(const std::string &app_name, WarmStartState &s
      */
     state = WSUNKNOWN;
 
-    for(std::map<WarmStart::WarmStartState, std::string>::const_iterator it = warmStartStateNameMap.begin(); it != warmStartStateNameMap.end(); it++)
+    for(auto it = warmStartStateNameMap.begin(); it != warmStartStateNameMap.end(); it++)
     {
-      if(it->second == statestr)
-      {
-         state = it->first;
-         break;
-      }
+        if(it->second == statestr)
+        {
+            state = it->first;
+            break;
+        }
     }
-
+        
     SWSS_LOG_INFO("%s warm start state get %s(%d)",
                     app_name.c_str(), statestr.c_str(), state);
 
     return;
 }
-
 
 // Set the WarmStart FSM state for a particular application.
 void WarmStart::setWarmStartState(const std::string &app_name, WarmStartState state)
