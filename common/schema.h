e0c229a361e8f56790f3011ee86277305e925f3c
mprabhu-nokia <66807480+mprabhu-nokia@users.noreply.github.com>
2020-11-04 12:20:56 -0500

    CHASSIS_STATE_DB on control-card for chassis state (#395)
    * GLOBAL_STATE_DB on control-card for chassis state
    
    On a modular chassis, add a GLOBAL_STATE_DB where all state
    information from line-cards can be persisted.
    
    One example is thermal sensors information from each of the line
    cards are stored in GLOBAL_STATE_DB. Control card can access this
    information to feed to the thermal-policy, fan algorithm etc.
    
    * Changing global to chassis for DB names

--- common/schema.h
+++ common/schema.h
@@ -20,7 +20,8 @@ namespace swss {
 #define GB_ASIC_DB      9
 #define GB_COUNTERS_DB  10
 #define GB_FLEX_COUNTER_DB  11
-#define CHASSIS_APP_DB  12
+#define CHASSIS_APP_DB      12
+#define CHASSIS_STATE_DB    13
 
 /***** APPLICATION DATABASE *****/
 
--- common/table.cpp
+++ common/table.cpp
@@ -31,7 +31,8 @@ const TableNameSeparatorMap TableBase::tableNameSeparatorMap = {
    { GB_ASIC_DB,          TABLE_NAME_SEPARATOR_VBAR  },
    { GB_COUNTERS_DB,      TABLE_NAME_SEPARATOR_VBAR  },
    { GB_FLEX_COUNTER_DB,  TABLE_NAME_SEPARATOR_VBAR  },
-   { CHASSIS_APP_DB,      TABLE_NAME_SEPARATOR_VBAR  }
+   { CHASSIS_APP_DB,      TABLE_NAME_SEPARATOR_VBAR  },
+   { CHASSIS_STATE_DB,    TABLE_NAME_SEPARATOR_VBAR  }
 };
 
 Table::Table(const DBConnector *db, const string &tableName)
