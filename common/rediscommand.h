13228866733bcbb971c76ba27da2e68c7bd4672d
Cosmin-Jinga-MS <80100666+Cosmin-Jinga-MS@users.noreply.github.com>
2021-09-09 08:13:42 +0300

    Update rediscommand.h
    Changed to int64 to fit full range of redis accepted input.

--- common/rediscommand.h
+++ common/rediscommand.h
@@ -51,7 +51,7 @@ public:
     void formatHDEL(const std::string& key, const std::vector<std::string>& fields);
 
     /* Format EXPIRE key field command */
-    void formatEXPIRE(const std::string& key, const int& ttl);
+    void formatEXPIRE(const std::string& key, const int64_t& ttl);
 
     const char *c_str() const;
 
