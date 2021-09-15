be91a95998f68b10f6eabcaa941a3328d7b19ef1
Preetham Singh <preetham.singh@broadcom.com>
2021-09-14 23:12:41 -0700

    Routed subinterface enhancements Support for long name and short name routed subinterfaces This swss common library provides APIS for: - Performs Subinterface validation checks - Get parent interface corresponding to subinterface(short/long name) - Get subinterface index

---
+++ common/ifcommon.cpp
@@ -0,0 +1,120 @@
+#include <cerrno>
+#include <cstring>
+#include <array>
+#include <net/if.h>
+#include "ifcommon.h"
+
+using namespace swss;
+
+subIntf::subIntf(const std::string &ifName)
+{
+    alias = ifName;
+	size_t found = alias.find(VLAN_SUB_INTERFACE_SEPARATOR);
+	if (found != std::string::npos)
+	{
+		parentIf = alias.substr(0, found);
+		if (!parentIf.compare(0, strlen("Eth"), "Eth"))
+		{
+            std::string parentIfName;
+			if (!parentIf.compare(0, strlen("Ethernet"), "Ethernet"))
+			{
+		        parentIfShortName = "Eth" + parentIf.substr(strlen("Ethernet"));
+			    isCompressed = false;
+			    parentIfName = "Ethernet" + parentIf.substr(strlen("Ethernet"));
+			}
+			else
+			{
+		        parentIfShortName = "Eth" + parentIf.substr(strlen("Eth"));
+				isCompressed = true;
+			    parentIfName = "Ethernet" + parentIf.substr(strlen("Eth"));
+			}
+            parentIf = parentIfName;
+		    subIfIdx = alias.substr(found + 1);
+		}
+		else if (!parentIf.compare(0, strlen("Po"), "Po"))
+		{
+			if (!parentIf.compare(0, strlen("PortChannel"), "PortChannel"))
+			{
+				isCompressed = false;
+		        parentIfShortName = "Po" + parentIf.substr(strlen("PortChannel"));
+			    parentIf = "PortChannel" + parentIf.substr(strlen("PortChannel"));
+			}
+			else
+			{
+				isCompressed = true;
+		        parentIfShortName = "Po" + parentIf.substr(strlen("Po"));
+			    parentIf = "PortChannel" + parentIf.substr(strlen("Po"));
+			}
+		    subIfIdx = alias.substr(found + 1);
+		}
+		else
+		{
+			subIfIdx = "";
+		}
+	}
+}
+
+bool subIntf::isValid()
+{
+	if (subIfIdx == "")
+    {
+		return false;
+    }
+    else if (alias.length() >= IFNAMSIZ)
+    {
+        return false;
+    }
+
+	return true;
+}
+
+std::string subIntf::parentIntf()
+{
+	return parentIf;
+}
+
+int subIntf::subIntfIdx()
+{
+    uint16_t id;
+    try
+    {
+        id = static_cast<uint16_t>(stoul(subIfIdx));
+    }
+    catch (const std::invalid_argument &e)
+    {
+        return -1;
+    }
+    catch (const std::out_of_range &e)
+    {
+        return -1;
+    }
+	return id;
+}
+
+std::string subIntf::longName()
+{
+	if (isValid() == false)
+		return "";
+
+	return (parentIf + VLAN_SUB_INTERFACE_SEPARATOR + subIfIdx);
+}
+
+std::string subIntf::shortName()
+{
+	if (isValid() == false)
+		return "";
+
+	return (parentIfShortName + VLAN_SUB_INTERFACE_SEPARATOR + subIfIdx);
+}
+
+bool subIntf::isShortName()
+{
+    if (isCompressed == true)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
