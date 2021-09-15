e34fe85f20b733e50846dda5445e775bbe1a52b5
Preetham Singh <preetham.singh@broadcom.com>
2021-09-14 23:23:36 -0700

    Indentation fix

--- common/ifcommon.cpp
+++ common/ifcommon.cpp
@@ -9,68 +9,68 @@ using namespace swss;
 subIntf::subIntf(const std::string &ifName)
 {
     alias = ifName;
-	size_t found = alias.find(VLAN_SUB_INTERFACE_SEPARATOR);
-	if (found != std::string::npos)
-	{
-		parentIf = alias.substr(0, found);
-		if (!parentIf.compare(0, strlen("Eth"), "Eth"))
-		{
+    size_t found = alias.find(VLAN_SUB_INTERFACE_SEPARATOR);
+    if (found != std::string::npos)
+    {
+        parentIf = alias.substr(0, found);
+        if (!parentIf.compare(0, strlen("Eth"), "Eth"))
+        {
             std::string parentIfName;
-			if (!parentIf.compare(0, strlen("Ethernet"), "Ethernet"))
-			{
-		        parentIfShortName = "Eth" + parentIf.substr(strlen("Ethernet"));
-			    isCompressed = false;
-			    parentIfName = "Ethernet" + parentIf.substr(strlen("Ethernet"));
-			}
-			else
-			{
-		        parentIfShortName = "Eth" + parentIf.substr(strlen("Eth"));
-				isCompressed = true;
-			    parentIfName = "Ethernet" + parentIf.substr(strlen("Eth"));
-			}
+            if (!parentIf.compare(0, strlen("Ethernet"), "Ethernet"))
+            {
+                parentIfShortName = "Eth" + parentIf.substr(strlen("Ethernet"));
+                isCompressed = false;
+                parentIfName = "Ethernet" + parentIf.substr(strlen("Ethernet"));
+            }
+            else
+            {
+                parentIfShortName = "Eth" + parentIf.substr(strlen("Eth"));
+                isCompressed = true;
+                parentIfName = "Ethernet" + parentIf.substr(strlen("Eth"));
+            }
             parentIf = parentIfName;
-		    subIfIdx = alias.substr(found + 1);
-		}
-		else if (!parentIf.compare(0, strlen("Po"), "Po"))
-		{
-			if (!parentIf.compare(0, strlen("PortChannel"), "PortChannel"))
-			{
-				isCompressed = false;
-		        parentIfShortName = "Po" + parentIf.substr(strlen("PortChannel"));
-			    parentIf = "PortChannel" + parentIf.substr(strlen("PortChannel"));
-			}
-			else
-			{
-				isCompressed = true;
-		        parentIfShortName = "Po" + parentIf.substr(strlen("Po"));
-			    parentIf = "PortChannel" + parentIf.substr(strlen("Po"));
-			}
-		    subIfIdx = alias.substr(found + 1);
-		}
-		else
-		{
-			subIfIdx = "";
-		}
-	}
+            subIfIdx = alias.substr(found + 1);
+        }
+        else if (!parentIf.compare(0, strlen("Po"), "Po"))
+        {
+            if (!parentIf.compare(0, strlen("PortChannel"), "PortChannel"))
+            {
+                isCompressed = false;
+                parentIfShortName = "Po" + parentIf.substr(strlen("PortChannel"));
+                parentIf = "PortChannel" + parentIf.substr(strlen("PortChannel"));
+            }
+            else
+            {
+                isCompressed = true;
+                parentIfShortName = "Po" + parentIf.substr(strlen("Po"));
+                parentIf = "PortChannel" + parentIf.substr(strlen("Po"));
+            }
+            subIfIdx = alias.substr(found + 1);
+        }
+        else
+        {
+            subIfIdx = "";
+        }
+    }
 }
 
 bool subIntf::isValid()
 {
-	if (subIfIdx == "")
+    if (subIfIdx == "")
     {
-		return false;
+        return false;
     }
     else if (alias.length() >= IFNAMSIZ)
     {
         return false;
     }
 
-	return true;
+    return true;
 }
 
 std::string subIntf::parentIntf()
 {
-	return parentIf;
+    return parentIf;
 }
 
 int subIntf::subIntfIdx()
@@ -88,23 +88,23 @@ int subIntf::subIntfIdx()
     {
         return -1;
     }
-	return id;
+    return id;
 }
 
 std::string subIntf::longName()
 {
-	if (isValid() == false)
-		return "";
+    if (isValid() == false)
+        return "";
 
-	return (parentIf + VLAN_SUB_INTERFACE_SEPARATOR + subIfIdx);
+    return (parentIf + VLAN_SUB_INTERFACE_SEPARATOR + subIfIdx);
 }
 
 std::string subIntf::shortName()
 {
-	if (isValid() == false)
-		return "";
+    if (isValid() == false)
+        return "";
 
-	return (parentIfShortName + VLAN_SUB_INTERFACE_SEPARATOR + subIfIdx);
+    return (parentIfShortName + VLAN_SUB_INTERFACE_SEPARATOR + subIfIdx);
 }
 
 bool subIntf::isShortName()
--- common/ifcommon.h
+++ common/ifcommon.h
@@ -2,25 +2,24 @@
 #define __IFCOMMON_H__
 
 namespace swss {
-	class subIntf
-	{
-		public:
-			subIntf(const std::string &ifName);
-			bool isValid();
-			std::string parentIntf();
-			int subIntfIdx();
-			std::string longName();
-			std::string shortName();
-			bool isShortName();
-
+    class subIntf
+    {
+        public:
+            subIntf(const std::string &ifName);
+            bool isValid();
+            std::string parentIntf();
+            int subIntfIdx();
+            std::string longName();
+            std::string shortName();
+            bool isShortName();
         private:
 #define VLAN_SUB_INTERFACE_SEPARATOR   "."
             std::string alias = "";
-			std::string subIfIdx = "";
-			std::string parentIf = "";
-			std::string parentIfShortName = "";
-			bool isCompressed = false;
-	};
+            std::string subIfIdx = "";
+            std::string parentIf = "";
+            std::string parentIfShortName = "";
+            bool isCompressed = false;
+    };
 }
 
 #endif
