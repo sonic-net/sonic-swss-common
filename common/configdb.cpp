c726e40fb1f16bbdb4a1973c3918f44d2711ffab
liuh-80 <liuh@microsoft.com>
2022-03-31 08:22:51 +0000

    Add file change

--- common/configdb.cpp
+++ common/configdb.cpp
@@ -5,8 +5,132 @@
 #include "pubsub.h"
 #include "converter.h"
 
+#include <dirent.h> 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <libyang/libyang.h>
+
 using namespace std;
 using namespace swss;
+
+map<string, lys_node *> table_name_to_data_mapping;
+
+struct ly_ctx * CreateCtxAndLoadAllModules(char* module_path)
+{
+    struct ly_ctx *ctx = ly_ctx_new(module_path, LY_CTX_ALLIMPLEMENTED);
+    DIR *module_dir = opendir(module_path);
+    if (module_dir)
+    {
+        struct dirent *sub_dir;
+        while ((sub_dir = readdir(module_dir)) != NULL)
+        {
+            if (sub_dir->d_type == DT_REG)
+            {
+                string file_name(sub_dir->d_name);
+                printf("file_name: %s\n", file_name.c_str());
+                auto pos = file_name.find(".yang");
+                string module_name = file_name.substr(0, pos);
+                printf("%s\n", module_name.c_str());
+
+                // load module
+                const struct lys_module *module = ly_ctx_load_module(ctx, module_name.c_str(), "");
+                if (module->data)
+                {
+                    // every module should always has a top level container same with module name
+                    // https://github.com/Azure/SONiC/blob/eeebbf6f8a32e5d989595b0816849e8ef0d15141/doc/mgmt/SONiC_YANG_Model_Guidelines.md
+                    struct lys_node *data = module->data;
+                    printf("root node name: %s\n",data->name);
+                    data = data->child;
+                    string container_name(data->name);
+                    printf("root container name: %s\n",data->name);
+                    table_name_to_data_mapping[container_name] = data;
+                }
+            }
+        }
+        closedir(module_dir);
+    }
+    
+    return ctx;
+}
+
+void LoadAndAppendDefaultValue(string module_name, map<string, map<string, string>>& data)
+{
+    CreateCtxAndLoadAllModules("/home/liuh/yang/yang-models/");
+
+    auto module_root_container = table_name_to_data_mapping.find(module_name);
+    if (module_root_container == table_name_to_data_mapping.end())
+    {
+        return;
+    }
+    
+    // found yang model for the table
+    auto next_child = module_root_container->second->child;
+    while (next_child)
+    {
+        printf("Child name: %s\n",next_child->name);
+        switch (next_child->nodetype)
+        {
+            case LYS_LIST:
+            {
+                // Mapping tables in Redis are defined using nested 'list'. Use 'sonic-ext:map-list "true";' to indicate that the 'list' is used for mapping table. The outer 'list' is used for multiple instances of mapping. The inner 'list' is used for mapping entries for each outer list instance.
+                // check if the list if for mapping ConfigDbTable
+                printf("Ext count: %d\n",next_child->ext_size);
+                
+                auto table_column = next_child->child;
+                while (table_column)
+                {
+                    struct lys_node_leaf *leafnode = (struct lys_node_leaf*)table_column;
+                    string column_name(table_column->name);
+                    printf("    table column name: %s, default: %s\n",table_column->name, leafnode->dflt);
+                    
+                    // assign default value to every row
+                    if (leafnode->dflt)
+                    {
+                        string default_value(leafnode->dflt);
+                        for (auto row_iterator = data.begin(); row_iterator != data.end(); ++row_iterator)
+                        {
+                            printf("assign default data to: %s\n", row_iterator->first.c_str());
+                            auto column = row_iterator->second.find(column_name);
+                            if (column == row_iterator->second.end())
+                            {
+                                printf("assigned default data to %s: %s\n", column_name.c_str(), default_value.c_str());
+                                // when no data from config DB, assign default value to result
+                                row_iterator->second[column_name] = default_value;
+                            }
+                        }
+                    }
+                    
+                    table_column = table_column->next;
+                }
+            }
+            break;
+            case LYS_UNKNOWN:
+            case LYS_CONTAINER:
+            case LYS_CHOICE:
+            case LYS_LEAF:
+            case LYS_LEAFLIST:
+            case LYS_ANYXML:
+            case LYS_CASE:
+            case LYS_NOTIF:
+            case LYS_RPC:
+            case LYS_INPUT:
+            case LYS_OUTPUT:
+            case LYS_GROUPING:
+            case LYS_USES:
+            case LYS_AUGMENT:
+            case LYS_ACTION:
+            case LYS_ANYDATA:
+            case LYS_EXT:
+            default:
+            break;
+        }
+
+        next_child = next_child->next;
+    }
+}
 
 ConfigDBConnector_Native::ConfigDBConnector_Native(bool use_unix_socket_path, const char *netns)
     : SonicV2Connector_Native(use_unix_socket_path, netns)
@@ -323,63 +447,6 @@ void ConfigDBPipeConnector_Native::_delete_table(DBConnector& client, RedisTrans
     while (cur != 0)
     {
         cur = _delete_entries(client, pipe, pattern.c_str(), cur);
-    }
-}
-
-// Write a table entry to config db
-//    Remove extra fields in the db which are not in the data
-// Args:
-//     table: Table name
-//     key: Key of table entry, or a tuple of keys if it is a multi-key table
-//     data: Table row data in a form of dictionary {'column_key': 'value', ...}
-//           Pass {} as data will delete the entry
-void ConfigDBPipeConnector_Native::set_entry(string table, string key, const map<string, string>& data)
-{
-    auto& client = get_redis_client(m_db_name);
-    DBConnector clientPipe(client);
-    RedisTransactioner pipe(&clientPipe);
-
-    pipe.multi();
-    _set_entry(pipe, table, key, data);
-    pipe.exec();
-}
-
-// Write a table entry to config db
-//    Remove extra fields in the db which are not in the data
-// Args:
-//     pipe: Redis DB pipe
-//     table: Table name
-//     key: Key of table entry, or a tuple of keys if it is a multi-key table
-//     data: Table row data in a form of dictionary {'column_key': 'value', ...}
-//           Pass {} as data will delete the entry
-void ConfigDBPipeConnector_Native::_set_entry(RedisTransactioner& pipe, std::string table, std::string key, const std::map<std::string, std::string>& data)
-{
-    string _hash = to_upper(table) + m_table_name_separator + key;
-    if (data.empty())
-    {
-        RedisCommand sdel;
-        sdel.format("DEL %s", _hash.c_str());
-        pipe.enqueue(sdel, REDIS_REPLY_INTEGER);
-    }
-    else
-    {
-        auto original = get_entry(table, key);
-
-        RedisCommand shmset;
-        shmset.formatHMSET(_hash, data.begin(), data.end());
-        pipe.enqueue(shmset, REDIS_REPLY_STATUS);
-
-        for (auto& it: original)
-        {
-            auto& k = it.first;
-            bool found = data.find(k) != data.end();
-            if (!found)
-            {
-                RedisCommand shdel;
-                shdel.formatHDEL(_hash, k);
-                pipe.enqueue(shdel, REDIS_REPLY_INTEGER);
-            }
-        }
     }
 }
 
