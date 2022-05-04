a54c513283895790d7b03910f4854f890092689c
ntoorchi <niloofar@opennetworking.org>
2022-05-04 09:45:20 -0700

    clean up.

--- common/dbconnector.cpp
+++ common/dbconnector.cpp
@@ -237,28 +237,6 @@ std::unordered_map<std::string, SonicDBInfo> SonicDBConfig::getDbEntry(const std
 
 SonicDBInfo& SonicDBConfig::getDbInfo(const std::string &dbName, const std::string &netns)
 {
-    // std::lock_guard<std::recursive_mutex> guard(m_db_info_mutex);
-
-    // SWSS_LOG_ENTER();
-
-    // if (!m_init)
-    //     initialize(DEFAULT_SONIC_DB_CONFIG_FILE);
-
-    // if (!netns.empty())
-    // {
-    //     if (!m_global_init)
-    //     {
-    //         SWSS_LOG_THROW("Initialize global DB config using API SonicDBConfig::initializeGlobalConfig");
-    //     }
-    // }
-    // auto foundNetns = m_db_info.find(netns);
-    // if (foundNetns == m_db_info.end())
-    // {
-    //     string msg = "Namespace " + netns + " is not a valid namespace name in config file";
-    //     SWSS_LOG_ERROR("%s", msg.c_str());
-    //     throw out_of_range(msg);
-    // }
-    // auto& infos = foundNetns->second;
     auto infos = getDbEntry(netns);
     auto foundDb = infos.find(dbName);
     if (foundDb == infos.end())
@@ -316,29 +294,6 @@ int SonicDBConfig::getDbId(const string &dbName, const string &netns)
 
 string SonicDBConfig::getDbName(int dbId, const string &netns)
 {
-    // std::lock_guard<std::recursive_mutex> guard(m_db_info_mutex);
-
-    // if (!m_init)
-    //     initialize(DEFAULT_SONIC_DB_CONFIG_FILE);
-
-    // if (!netns.empty())
-    // {
-    //     if (!m_global_init)
-    //     {
-    //         SWSS_LOG_THROW("Initialize global DB config using API SonicDBConfig::initializeGlobalConfig");
-    //     }
-    // }
-
-    // auto foundNetns = m_db_info.find(netns);
-    // if (foundNetns == m_db_info.end())
-    // {
-    //     string msg = "Namespace " + netns + " is not a valid namespace name in config file";
-    //     SWSS_LOG_ERROR("%s", msg.c_str());
-    //     throw out_of_range(msg);
-    // }
-    
-    // string db_name;
-    // auto& infos = foundNetns->second;
     string db_name;
     auto infos = getDbEntry(netns);
     for ( auto it = infos.begin(); it != infos.end(); ++it ) {
