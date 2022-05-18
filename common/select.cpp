e2c690b5f9fc392dae080f475ef2b8b26936de16
liuh-80 <azureuser@liuh-dev-vm-02.5fg3zjdzj2xezlx1yazx5oxkzd.hx.internal.cloudapp.net>
2022-05-18 08:36:45 +0000

    Fix code issue

--- common/select.cpp
+++ common/select.cpp
@@ -120,7 +120,7 @@ int Select::poll_descriptors(Selectable **c, unsigned int timeout)
     }
 
     bool dataReady = false;
-    foreach (auto sel : temp_ready)
+    for (auto sel : temp_ready)
     {
         // we must update clock only when the selector out of the m_ready
         // otherwise we break invariant of the m_ready
