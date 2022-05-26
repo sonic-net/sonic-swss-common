675d7931b074f18a314c760def60af17b5a00a64
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2022-05-26 09:34:41 +0800

    Re-write sonic-db-cli with c++ for sonic startup performance issue (#607)
    #### Why I did it
        Fix sonic-db-cli high CPU usage on SONiC startup issue: https://github.com/Azure/sonic-buildimage/issues/10218
        ETA of this issue will be 2022/05/31
    
    #### How I did it
        Re-write sonic-cli with c++.
    
    #### How to verify it
        Add c++ unit test to cover all code.
        Pass all E2E test scenario.
    
    #### Which release branch to backport (provide reason below if selected)
    
    <!--
    - Note we only backport fixes to a release branch, *not* features!
    - Please also provide a reason for the backporting below.
    - e.g.
    - [x] 202006
    -->
    
    - [ ] 201811
    - [ ] 201911
    - [ ] 202006
    - [ ] 202012
    - [ ] 202106
    - [ ] 202111
    
    #### Description for the changelog
        Re-write sonic-cli with c++ for sonic startup performance issue

---
+++ sonic-db-cli/sonic-db-cli.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <functional>
+#include <string>
+#include <vector>
+#include <memory>
+#include "common/dbconnector.h"
+#include "common/dbinterface.h"
+#include "common/redisreply.h"
+
+struct Options
+{
+    bool m_help = false;
+    bool m_unixsocket = false;
+    std::string m_namespace;
+    std::string m_db_or_op;
+    std::vector<std::string> m_cmd;
+};
+
+void printUsage();
+
+void printRedisReply(swss::RedisReply& reply);
+
+std::shared_ptr<swss::DBConnector> connectDbInterface(
+    const std::string& db_name,
+    const std::string& netns,
+    bool isTcpConn);
+
+int executeCommands(
+    const std::string& db_name,
+    std::vector<std::string>& commands,
+    const std::string& netns,
+    bool isTcpConn);
+
+std::string handleSingleOperation(
+    const std::string& netns,
+    const std::string& db_name,
+    const std::string& operation,
+    bool isTcpConn);
+
+int handleAllInstances(
+    const std::string& netns,
+    const std::string& operation,
+    bool isTcpConn);
+
+void parseCliArguments(
+    int argc,
+    char** argv,
+    Options &options);
+
+int sonic_db_cli(
+    int argc,
+    char** argv,
+    std::function<void()> initializeGlobalConfig,
+    std::function<void()> initializeConfig);
