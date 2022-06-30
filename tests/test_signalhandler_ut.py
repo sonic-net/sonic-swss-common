af80c12955ef0aafcce816fd58783a4edf979266
Stepan Blyshchak <38952541+stepanblyschak@users.noreply.github.com>
2022-06-30 20:19:19 +0300

    [select] break the select loop if interrupt_on_signal flag is set (#624)

--- tests/test_signalhandler_ut.py
+++ tests/test_signalhandler_ut.py
@@ -1,33 +1,34 @@
 import signal
 import os
 import pytest
+import multiprocessing
+import time
 from swsscommon import swsscommon
-from swsscommon.swsscommon import SignalHandlerHelper
 
-def dummy_signal_handler(signum, stack):
-    # ignore signal so UT will not break
-    pass
+def test_config_db_listen_while_signal_received():
+    """ Test performs ConfigDBConnector.listen() while signal is received,
+    checks that the listen() call is interrupted and the regular KeyboardInterrupt is raised.
+    """
+    c=swsscommon.ConfigDBConnector()
+    c.subscribe('A', lambda a: None)
+    c.connect(wait_for_init=False)
+    event = multiprocessing.Event()
 
-def test_SignalHandler():
-    signal.signal(signal.SIGUSR1, dummy_signal_handler)
+    def signal_handler(signum, frame):
+        event.set()
+        sys.exit(0)
 
-    # Register SIGUSER1
-    SignalHandlerHelper.registerSignalHandler(signal.SIGUSR1)
-    happened = SignalHandlerHelper.checkSignal(signal.SIGUSR1)
-    assert happened == False
+    signal.signal(signal.SIGUSR1, signal_handler)
 
-    # trigger SIGUSER manually
-    os.kill(os.getpid(), signal.SIGUSR1)
-    happened = SignalHandlerHelper.checkSignal(signal.SIGUSR1)
-    assert happened == True
-    
-    # Reset signal
-    SignalHandlerHelper.resetSignal(signal.SIGUSR1)
-    happened = SignalHandlerHelper.checkSignal(signal.SIGUSR1)
-    assert happened == False
-    
-    # un-register signal handler
-    SignalHandlerHelper.restoreSignalHandler(signal.SIGUSR1)
-    os.kill(os.getpid(), signal.SIGUSR1)
-    happened = SignalHandlerHelper.checkSignal(signal.SIGUSR1)
-    assert happened == False
+    def listen():
+        c.listen()
+
+    thr = multiprocessing.Process(target=listen)
+    thr.start()
+
+    time.sleep(5)
+    os.kill(thr.pid, signal.SIGUSR1)
+
+    thr.join()
+
+    assert event.is_set()
