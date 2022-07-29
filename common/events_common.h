bb777f3e649be62487a3854a31903e61ac403dc6
Renuka Manavalan <remanava@microsoft.com>
2022-07-29 23:04:43 +0000

    compile fix

--- common/events_common.h
+++ common/events_common.h
@@ -321,7 +321,7 @@ struct serialization
         else {
             /* override with zmq err */
             rc = zmq_errno();
-            RET_ON_ERR(false, "Failed to read part rc=%d", rc);
+            RET_ON_ERR(rc == 11, "Failed to read part rc=%d", rc);
         }
     out:
         zmq_msg_close(&msg);
@@ -381,7 +381,7 @@ struct serialization
              */
             rc2 = zmq_read_part(sock, 0, more, pt2);
         }
-        RET_ON_ERR(rc == 0, "Failed to read part1");
+        RET_ON_ERR((rc == 0) || (rc == 11), "Failed to read part1");
         if (rc2 != 0) {
             rc = rc2;
             RET_ON_ERR(false, "Failed to read part2");
