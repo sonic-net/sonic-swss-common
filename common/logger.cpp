f89ff3350882844fffa84d78c7abc56b338d00ce
pettershao-ragilenetworks <81281940+pettershao-ragilenetworks@users.noreply.github.com>
2022-09-20 13:46:21 +0800

    Update logger.cpp

--- common/logger.cpp
+++ common/logger.cpp
@@ -233,9 +233,7 @@ void Logger::settingThread()
         }
 
         KeyOpFieldsValuesTuple koValues;
-
-        ConsumerStateTable *consumerStateTable;
-        consumerStateTable = dynamic_cast<ConsumerStateTable *>(selectable);
+        ConsumerStateTable *consumerStateTable = dynamic_cast<ConsumerStateTable *>(selectable);
         if (consumerStateTable == NULL)
         {
             continue;
