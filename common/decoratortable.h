fb7fbaf505bbb86aaccc00c0155aec3514fc7e0e
liuh-80 <liuh@microsoft.com>
2022-11-21 03:38:14 +0000

    Add default value decorators

---
+++ common/decoratortable.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <assert.h>
+#include <string>
+#include <queue>
+#include <tuple>
+#include <utility>
+#include <map>
+#include <memory>
+#include <deque>
+#include "table.h"
+#include "subscriberstatetable.h"
+#include "defaultvalueprovider.h"
+
+namespace swss {
+
+class DecoratorTable : public Table  
+{
+public:
+    DecoratorTable(const DBConnector *db, const std::string &tableName);
+    DecoratorTable(RedisPipeline *pipeline, const std::string &tableName, bool buffered);
+    ~DecoratorTable() override;
+
+    /* Get all the field-value tuple of the table entry with the key */
+    bool get(const std::string &key, std::vector<std::pair<std::string, std::string>> &ovalues) override;
+
+    /* Get an entry field-value from the table */
+    bool hget(const std::string &key, const std::string &field,  std::string &value) override;
+
+private:
+    DefaultValueProvider *m_defaultValueProvider;
+};
+
+}
