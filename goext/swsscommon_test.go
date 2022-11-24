2c0ff4a279f5478b05fab40f0cfea45b030d4d87
ganglyu <ganglv@microsoft.com>
2022-11-24 03:07:25 +0000

    Add UT for producer state table

--- goext/swsscommon_test.go
+++ goext/swsscommon_test.go
@@ -11,3 +11,26 @@ func TestSonicDBConfig(t *testing.T) {
 	SonicDBConfigInitialize(filepath.Dir(pwd) + "/tests/redis_multi_db_ut_config/database_config.json")
 }
 
+func TestProducerStateTable(t *testing.T) {
+	db := NewDBConnector("APPL_DB", uint(0), true)
+	pt := NewProducerStateTable(db, "TEST_TABLE")
+	tbl := NewTable(db, "_TEST_TABLE")
+	vec := NewFieldValuePairs()
+	key := "aaa"
+	pair := NewFieldValuePair("a", "b")
+	vec.Add(pair)
+	pt.Set(key, vec, "SET", "")
+	fvs := NewFieldValuePairs()
+	ret := tbl.Get("aaa", fvs)
+	if ret != true {
+		t.Errorf("Get table failed")
+	}
+	fv := fvs.Get(0)
+	if fv.GetFirst() != "a" {
+		t.Errorf("Wronge fv first %v", fv.GetFirst())
+	}
+	if fv.GetSecond() != "b" {
+		t.Errorf("Wronge fv second: %v", fv.GetSecond())
+	}
+}
+
