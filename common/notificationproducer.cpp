6b6842a9690507d300a5beb4c0baff47094c94a5
Stepan Blyshchak <38952541+stepanblyschak@users.noreply.github.com>
2023-01-30 19:44:29 +0200

    [NotificationProducer] add pipeline support (#708)
    Added a pipeline support for *NotificationProducer*. *NotificationProducer* created with *RedisPipeline* will buffer published notifications untill a *flush* is called on *RedisPipeline* or *RedisPipeline*'s command buffer is filled.
    
    The motivation for this change is to optimize redis usage when using *ResponsePublisher* from sonic-swss for a lot of entries.

--- common/notificationproducer.cpp
+++ common/notificationproducer.cpp
@@ -1,7 +1,14 @@
 #include "notificationproducer.h"
 
+#define NON_BUFFERED_COMMAND_BUFFER_SIZE 1
+
 swss::NotificationProducer::NotificationProducer(swss::DBConnector *db, const std::string &channel):
-    m_db(db), m_channel(channel)
+    m_ownedpipe(std::make_unique<swss::RedisPipeline>(db, NON_BUFFERED_COMMAND_BUFFER_SIZE)), m_pipe(m_ownedpipe.get()), m_channel(channel), m_buffered(false)
+{
+}
+
+swss::NotificationProducer::NotificationProducer(swss::RedisPipeline *pipeline, const std::string &channel, bool buffered):
+     m_pipe(pipeline), m_channel(channel), m_buffered(buffered)
 {
 }
 
@@ -19,5 +26,19 @@ int64_t swss::NotificationProducer::send(const std::string &op, const std::strin
 
     SWSS_LOG_DEBUG("channel %s, publish: %s", m_channel.c_str(), msg.c_str());
 
-    return m_db->publish(m_channel, msg);
+    RedisCommand command;
+    command.format("PUBLISH %s %s", m_channel.c_str(), msg.c_str());
+
+    if (m_buffered)
+    {
+        m_pipe->push(command, REDIS_REPLY_INTEGER);
+
+        // if operating in buffered mode return -1 as we can't know the number
+        // of client's that have read the message immediately
+        return -1;
+    }
+
+    RedisReply reply = m_pipe->push(command);
+    reply.checkReplyType(REDIS_REPLY_INTEGER);
+    return reply.getReply<long long int>();
 }
