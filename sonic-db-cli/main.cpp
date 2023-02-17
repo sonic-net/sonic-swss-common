55a6dcf6924e0d494cfe51eaca2f297914ff307c
liuh-80 <liuh@microsoft.com>
2023-02-17 06:46:28 +0000

    Improve code coverage

--- sonic-db-cli/main.cpp
+++ sonic-db-cli/main.cpp
@@ -17,28 +17,9 @@ int main(int argc, char** argv)
         SonicDBConfig::initialize(SonicDBConfig::DEFAULT_SONIC_DB_CONFIG_FILE);
     };
 
-
-    try
-    {
-        return sonic_db_cli(
-                        argc,
-                        argv,
-                        initializeGlobalConfig,
-                        initializeConfig);
-    }
-    catch (const exception& e)
-    {
-        // sonic-db-cli is porting from python version.
-        // in python version, when any exception happen sonic-db-cli will crash but will not generate core dump file.
-        // catch all exception here to avoid a core dump file generated.
-        cerr << "An exception of type " << e.what() << " occurred. Arguments:" << endl;
-        for (int idx = 0; idx < argc; idx++)
-        {
-            cerr << argv[idx]  << " ";
-        }
-        cerr << endl;
-
-        // when python version crash, exit code is 1.
-        return 1;
-    }
+    return cli_exception_wrapper(
+                    argc,
+                    argv,
+                    initializeGlobalConfig,
+                    initializeConfig);
 }
