2f6cc6268299b86bb552da88408873a7b4bc8d7c
Ze Gan <ganze718@gmail.com>
2024-02-03 21:44:16 +0800

    Add API getDbKeys in dbconnector
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- common/dbconnector.cpp
+++ common/dbconnector.cpp
@@ -447,6 +447,22 @@ vector<string> SonicDBConfig::getNamespaces()
     return vector<string>(list.begin(), list.end());
 }
 
+vector<SonicDBKey> SonicDBConfig::getDbKeys()
+{
+    vector<SonicDBKey> keys;
+    std::lock_guard<std::recursive_mutex> guard(m_db_info_mutex);
+
+    if (!m_init)
+        initialize(DEFAULT_SONIC_DB_CONFIG_FILE);
+
+    // This API returns back all db keys.
+    for (auto it = m_inst_info.cbegin(); it != m_inst_info.cend(); ++it) {
+        keys.push_back(it->first);
+    }
+
+    return keys;
+}
+
 std::vector<std::string> SonicDBConfig::getDbList(const std::string &netns, const std::string &containerName)
 {
     SonicDBKey key;
