55d3df645e7c8464a4e5d20a76b6beace48675a4
Ze Gan <ganze718@gmail.com>
2024-05-29 00:34:12 +0800

    [swig]: Fix swig template memory leak on issue 17025  (#876)
    Fix the issue sonic-net/sonic-buildimage#17025 about Redis set activity
    
    Description
    The issue reports a memory leak on the Redis set operations
    
    Reason
    Didn't decrease the reference count after PySequence_GetItem
    Use the inappropriate Swig API and didn't release the string of SWIG_AsPtr_std_string
    Fix:
    Refer PR: Fix swig template memory leak #859 from @praveenraja1
    Replace the API SWIG_AsPtr_std_string to SWIG_AsVal_std_string
    Add unit test
    To monitor there is no dramatic memory increment after a huge amount of Redis set operations.
    
    Signed-off-by: Ze Gan <zegan@microsoft.com>

--- tests/test_redis_ut.py
+++ tests/test_redis_ut.py
@@ -831,3 +831,30 @@ def test_TableOpsMemoryLeak():
         t.get("long_data")
     assert psutil.Process(os.getpid()).memory_info().rss - rss < OP_COUNT
 
+
+def test_TableSetBinary():
+    app_db = swsscommon.DBConnector("APPL_DB", 0, True)
+    t = swsscommon.Table(app_db, "TABLE")
+    buff = b""
+    for i in range(0, 256):
+        buff += bytes([i])
+    buff = buff.decode('latin-1')
+    fvs = swsscommon.FieldValuePairs([("binary", buff)])
+    t.set("binary", fvs)
+    (status, fvs) = t.get("binary")
+    assert status == True
+    assert fvs[0][1] == buff
+
+
+def test_TableOpsMemoryLeak():
+    OP_COUNT = 50000
+    app_db = swsscommon.DBConnector("APPL_DB", 0, True)
+    t = swsscommon.Table(app_db, "TABLE")
+    long_data = "x" * 100
+    fvs = swsscommon.FieldValuePairs([(long_data, long_data)])
+    rss = psutil.Process(os.getpid()).memory_info().rss
+    for _ in range(OP_COUNT):
+        t.set("long_data", fvs)
+        t.get("long_data")
+    assert psutil.Process(os.getpid()).memory_info().rss - rss < OP_COUNT
+
