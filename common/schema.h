d319e70f30f6a8c3d2100a727158227bc8dd8fea
Stephen Sun <stephens@nvidia.com>
2024-10-25 09:54:18 +0300

    bulk chunk size
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- common/schema.h
+++ common/schema.h
@@ -283,6 +283,7 @@ namespace swss {
 #define LUA_PLUGIN_TYPE                 "LUA_PLUGIN_TYPE"
 #define SAI_OBJECT_TYPE                 "SAI_OBJECT_TYPE"
 
+#define BULK_CHUNK_SIZE_FIELD               "BULK_CHUNK_SIZE"
 #define POLL_INTERVAL_FIELD                 "POLL_INTERVAL"
 #define STATS_MODE_FIELD                    "STATS_MODE"
 #define STATS_MODE_READ                     "STATS_MODE_READ"
