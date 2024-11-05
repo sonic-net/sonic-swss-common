056aa77dc8abb5ca76b0280bf0e35c858114bd23
Stepan Blyshchak <38952541+stepanblyschak@users.noreply.github.com>
2024-11-05 18:33:33 +0200

    rename to isInterfaceNameValid in tests

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -2,7 +2,7 @@ from swsscommon import swsscommon
 
 def test_is_interface_name_valid():
     invalid_interface_name = "TooLongInterfaceName"
-    assert not swsscommon.is_interface_name_valid(invalid_interface_name)
+    assert not swsscommon.isInterfaceNameValid(invalid_interface_name)
 
     validInterfaceName = "OkInterfaceName"
-    assert swsscommon.is_interface_name_valid(validInterfaceName)
+    assert swsscommon.isInterfaceNameValid(validInterfaceName)
