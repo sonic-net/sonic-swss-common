acc4805a8c2766a4d200c17fdfaba04b94792274
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2024-11-08 09:36:21 +0800

    Fix the ZMQ producer/consumer table does not update changes to the database issue. (#942)
    #### Why I did it
    Fix a race condition bug: when deleting ZmqProducerStateTable immediately after setting/deleting data, the data is sent to ZMQ but not updated in the database.
    
    #### How I did it
    Verify and wait for the data update before the db update thread exits.
    
    ##### Work item tracking
    
    #### How to verify it
    Pass all test cases.
    
    #### Which release branch to backport (provide reason below if selected)
    
    <!--
    - Note we only backport fixes to a release branch, *not* features!
    - Please also provide a reason for the backporting below.
    - e.g.
    - [x] 202006
    -->
    
    - [ ] 201811
    - [ ] 201911
    - [ ] 202006
    - [ ] 202012
    - [ ] 202106
    - [ ] 202111
    
    #### Description for the changelog
    Fix the ZMQ producer/consumer table does not update changes to the database issue.
    
    #### Link to config_db schema for YANG module changes
    <!--
    Provide a link to config_db schema for the table for which YANG model
    is defined
    Link should point to correct section on https://github.com/Azure/SONiC/wiki/Configuration.
    -->
    
    #### A picture of a cute animal (not mandatory but encouraged)

--- tests/zmq_state_ut.cpp
+++ tests/zmq_state_ut.cpp
@@ -438,3 +438,30 @@ TEST(ZmqConsumerStateTableBatchBufferOverflow, test)
     }
     EXPECT_ANY_THROW(p.send(kcos));
 }
+
+TEST(ZmqProducerStateTableDeleteAfterSend, test)
+{
+    std::string testTableName = "ZMQ_PROD_DELETE_UT";
+    std::string pushEndpoint = "tcp://localhost:1234";
+    std::string pullEndpoint = "tcp://*:1234";
+    std::string testKey = "testKey";
+
+    ZmqServer server(pullEndpoint);
+
+    DBConnector db(TEST_DB, 0, true);
+    ZmqClient client(pushEndpoint);
+
+    auto *p = new ZmqProducerStateTable(&db, testTableName, client, true);
+    std::vector<FieldValueTuple> values;
+    FieldValueTuple t("test", "test");
+    values.push_back(t);
+    p->set(testKey,values);
+    delete p;
+
+    sleep(1);
+
+    Table table(&db, testTableName);
+    std::vector<std::string> keys;
+    table.getKeys(keys);
+    EXPECT_EQ(keys.front(), testKey);
+}
