ce4d8bd970fd36f4fe357347524ca5b669c09030
Kishore Kunal <kishore.kunal@broadcom.com>
2024-11-14 12:42:27 -0800

    [EVPN-MH] Added new APP_L2_NEXTHOP_GROUP_TABLE_NAME table in APPL_DB And support to register and de-register raw netlink msg
    Signed-off-by: Kishore Kunal <kishore.kunal@broadcom.com>
    Signed-off-by: Tamer Ahmed <tamerahmed@microsoft.com>

--- common/netdispatcher.cpp
+++ common/netdispatcher.cpp
@@ -35,10 +35,43 @@ void NetDispatcher::unregisterMessageHandler(int nlmsg_type)
     m_handlers.erase(it);
 }
 
+void NetDispatcher::registerRawMessageHandler(int nlmsg_type, NetMsg *callback)
+{
+    if (m_rawhandlers.find(nlmsg_type) != m_rawhandlers.end())
+        throw "Trying to registered on already registerd netlink message";
+
+    m_rawhandlers[nlmsg_type] = callback;
+}
+
+void NetDispatcher::unregisterRawMessageHandler(int nlmsg_type)
+{
+    auto it = m_rawhandlers.find(nlmsg_type);
+
+    if (it == m_rawhandlers.end())
+        throw "Trying to unregister non existing handler";
+
+    m_rawhandlers.erase(it);
+
+}
+
 void NetDispatcher::nlCallback(struct nl_object *obj, void *context)
 {
     NetMsg *callback = (NetMsg *)context;
     callback->onMsg(nl_object_get_msgtype(obj), obj);
+}
+
+void NetDispatcher::onNetlinkMessageRaw(struct nl_msg *msg)
+{
+    struct nlmsghdr *nlmsghdr = nlmsg_hdr(msg);
+    NetMsg *reg_callback = NULL;
+    auto callback = m_rawhandlers.find(nlmsghdr->nlmsg_type);
+
+    /* Drop not registered messages */
+    if (callback == m_rawhandlers.end())
+        return;
+
+    reg_callback = (NetMsg *)callback->second;
+    reg_callback->onMsgRaw(nlmsghdr);
 }
 
 NetMsg* NetDispatcher::getCallback(int nlmsg_type)
@@ -61,7 +94,9 @@ void NetDispatcher::onNetlinkMessage(struct nl_msg *msg)
 
     /* Drop not registered messages */
     if (callback == nullptr)
+    {
+        onNetlinkMessageRaw(msg);
         return;
-
+    }
     nl_msg_parse(msg, NetDispatcher::nlCallback, (callback));
 }
