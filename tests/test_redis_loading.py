23fe76eb6221d22fa91218e043b114814a88732f
Hua Liu <58683130+liuh-80@users.noreply.github.com>
2025-04-02 14:15:37 +0800

    Update test_redis_loading.py

--- tests/test_redis_loading.py
+++ tests/test_redis_loading.py
@@ -1,17 +1,18 @@
 import os
 import pytest
+import re
 import subprocess
 import threading
 import time
 from swsscommon import swsscommon
 
-TEST_TIMEOUT = 10
+TEST_TIMEOUT = 30
 REDIS_RECORD_COUNT = 1000000
 
-def restart_redis():
-    process = subprocess.Popen("sudo service redis-server restart", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def start_redis():
+    process = subprocess.Popen("sudo service redis-server start", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-    print("Command result: stdout={} stderr={}".format(stdout.decode(), stderr.decode()))
+    print("start redis result: stdout={} stderr={}".format(stdout.decode(), stderr.decode()))
 
 @pytest.fixture
 def loading_dataset():
@@ -26,8 +27,10 @@ def loading_dataset():
     # create dump file
     os.system("redis-cli save")
 
-    # restart redis server in a thread to reload dataset
-    thread = threading.Thread(target=restart_redis)
+    os.system("sudo service redis-server stop")
+
+    # start redis server in a thread to reload dataset
+    thread = threading.Thread(target=start_redis)
     thread.start()
 
     yield
@@ -46,7 +49,11 @@ def loading_dataset():
     # cleanup
     os.system("redis-cli FLUSHALL")
 
-def test_redis_loading_exception(loading_dataset):
+def test_redis_loading_exception(loading_dataset, capfd):
+    # write swss log to stderr, so capfd can capture it
+    swsscommon.Logger.swssOutputNotify("", "STDERR");
+    swsscommon.Logger.setMinPrio(swsscommon.Logger.SWSS_NOTICE);
+
     end_time = time.time() + TEST_TIMEOUT
     loading_exception = False
     while time.time() < end_time:
@@ -55,9 +62,17 @@ def test_redis_loading_exception(loading_dataset):
             db.keys("test")
         except Exception as e:
             error = "{}".format(e)
-            print("Exception: {}".format(error))
             if "LOADING" in error:
                 loading_exception = True
                 break
     
-    assert loading_exception, "LOADINg exception does not happen"
+    assert loading_exception, "LOADING exception does not happen"
+
+    pattern = r'.*WARN.*, reason: LOADING Redis is loading the dataset in memory.*'
+    captured_log = capfd.readouterr().err
+
+    with capfd.disabled():
+        print("captured syslog: {}".format(captured_log))
+
+    assert re.match(pattern, captured_log)
+    
