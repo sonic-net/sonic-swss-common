b44cde63a45d26b2ceaafd4df32bd40964e3d5ca
Qi Luo <qiluo-msft@users.noreply.github.com>
2025-05-27 05:56:29 +0000

    (reformat)

--- common/subscribereventtable.h
+++ common/subscribereventtable.h
@@ -7,7 +7,7 @@ namespace swss {
 class SubscriberEventTable : public ConsumerTableBase
 {
 public:
-    SubscriberEventTable (DBConnector *db, const std::string &tableName, int popBatchSize = DEFAULT_POP_BATCH_SIZE, int pri = 0);
+    SubscriberEventTable(DBConnector *db, const std::string &tableName, int popBatchSize = DEFAULT_POP_BATCH_SIZE, int pri = 0);
 
     /* Get all available events from pub/sub channel */
     void pops(std::deque<KeyOpFieldsValuesTuple> &vkco, const std::string &prefix = EMPTY_PREFIX);
