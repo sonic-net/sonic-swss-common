f588655b791041c9883a987037894692aa29648d
dypet <dypeters@cisco.com>
2025-07-22 15:07:52 -0400

    Add is_proto and convert_pb_to_json to SonicDbTable.

--- crates/swss-common/src/lib.rs
+++ crates/swss-common/src/lib.rs
@@ -23,5 +23,12 @@ pub trait SonicDbTable {
     fn key_separator() -> char;
     fn table_name() -> &'static str;
     fn db_name() -> &'static str;
+    fn is_proto() -> bool {
+        false
+    }
+    fn convert_pb_to_json(_kfv: &mut KeyOpFieldValues) {
+        // Default implementation does nothing.
+        // This can be overridden by the macro to convert protobuf to JSON.
+    }
     fn is_dpu() -> bool;
 }
