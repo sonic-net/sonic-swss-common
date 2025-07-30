a03890ae860655f59de562f144dcac3fc41b6340
David Pilnik <davidpil@nvidia.com>
2025-07-30 13:08:39 +0300

    Add REDIS_ACL flag support

--- common/dbconnector.cpp
+++ common/dbconnector.cpp
@@ -25,6 +25,9 @@ using namespace std;
 using namespace swss;
 #define SUDO_GID 27
 
+// This is a macro to check if the REDIS_ACL_G is defined, which meaning that the feature Redis ACL is enabled.
+#ifdef REDIS_ACL_G
+
 bool isUserInSudoGroup() {
     int ngroups = getgroups(0, nullptr); // Get the number of supplementary groups
     bool group_sudo_exists = false;
@@ -118,10 +121,11 @@ std::string get_auth_cmd() {
     return command;
 }
 
+#endif
+
 namespace {
 constexpr size_t KEY_DEL_CHUNK_SIZE = 128;
 }
-
 
 void SonicDBConfig::parseDatabaseConfig(const string &file,
                     std::map<std::string, RedisInstInfo> &inst_entry,
@@ -688,6 +692,7 @@ void RedisContext::initContext(const char *host, int port, const timeval *tv)
         throw system_error(make_error_code(errc::address_not_available),
                            "Unable to connect to redis - " + std::string(m_conn->errstr) + "(" + std::to_string(m_conn->err) + ")");
 
+#ifdef REDIS_ACL_G
     // Redis SSL configuration
     redisSSLContext *ssl;
     redisSSLContextError ssl_error = REDIS_SSL_CTX_NONE;
@@ -722,6 +727,7 @@ void RedisContext::initContext(const char *host, int port, const timeval *tv)
     std::string command = "";
     command = get_auth_cmd();
     RedisReply r1(this, command, REDIS_REPLY_STATUS);
+#endif
 }
 
 void RedisContext::initContext(const char *path, const timeval *tv)
@@ -739,10 +745,12 @@ void RedisContext::initContext(const char *path, const timeval *tv)
         throw system_error(make_error_code(errc::address_not_available),
                            "Unable to connect to redis (unix-socket) - " + std::string(m_conn->errstr) + "(" + std::to_string(m_conn->err) + ")");
 
+#ifdef REDIS_ACL_G
     // Redis Authentication
     std::string command = "";
     command = get_auth_cmd();
     RedisReply r1(this, command, REDIS_REPLY_STATUS);
+#endif
 }
 
 redisContext *RedisContext::getContext() const
