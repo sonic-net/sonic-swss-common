bb9573dcfc7a6fc5c682dd771eae7091743cf6c6
dypet <dypeters@cisco.com>
2025-07-31 12:10:32 -0400

    Remove SonicDbTable.

--- crates/swss-common/src/lib.rs
+++ crates/swss-common/src/lib.rs
@@ -17,18 +17,3 @@ pub fn sonic_db_config_initialize_global(path: &str) -> Result<(), Exception> {
     let path = cstr(path);
     unsafe { swss_try!(bindings::SWSSSonicDBConfig_initializeGlobalConfig(path.as_ptr())) }
 }
-
-/// Trait for objects that can be stored in a Sonic DB table.
-pub trait SonicDbTable {
-    fn key_separator() -> char;
-    fn table_name() -> &'static str;
-    fn db_name() -> &'static str;
-    fn is_proto() -> bool {
-        false
-    }
-    fn convert_pb_to_json(_kfv: &mut KeyOpFieldValues) {
-        // Default implementation does nothing.
-        // This can be overridden by the macro to convert protobuf to JSON.
-    }
-    fn is_dpu() -> bool;
-}
