74dbd4bb207ff75b7d3263b808138d598fc9a1a8
sonicbld <sonicbld@microsoft.com>
2025-10-13 03:24:34 +0000

    Save code

--- test_zmq_memory/main.cpp
+++ test_zmq_memory/main.cpp
@@ -1,98 +1,87 @@
-//#include "common/dbconnector.h"
-//#include "common/zmqproducerstatetable.h"
-//#include "common/table.h"
+#include "common/dbconnector.h"
+#include "common/zmqproducerstatetable.h"
+#include "common/table.h"
 #include <iostream>
 #include <string>
 #include <malloc.h>
 
-//#include "gperftools/malloc_extension.h"
+#include "route.pb.h"
+#include <chrono>
+#include "common/json.h"
 
-#include <string.h>
-#include <stdint.h>
-#include <vector>
-#include <queue>
-#include <list>
-#include <unistd.h>
-#include <errno.h>
-#include <system_error>
-#include <fstream>
-#include <set>
-#include <unistd.h>
+long serialize_duration_total = 0;
+long deserialize_duration_total = 0;
+long json_serialize_duration_total = 0;
+long json_deserialize_duration_total = 0;
 
-//#include "common/dbconnector.h"
-//#include "common/redisreply.h"
-//#include "common/redispipeline.h"
-//#include "common/pubsub.h"
-
-#include "json.hpp"
-#include "common/producertable.h"
-
-using json = nlohmann::json;
-using namespace std;
-
-
-int exit_press_space()
+void test()
 {
-    std::cout << "Press SPACE and then Enter to exit..." << std::endl;
-    std::string input;
-    std::getline(std::cin, input);
-    std::cout << "Exiting program." << std::endl;
-    return 0;
-}
-
-void set_mallopt()
-{
-    std::cout << "Set mallopt" << std::endl;
-    if (mallopt(M_MMAP_MAX, 1024) == -1) {
-        std::cout << "Set M_MMAP_MAX failed" << std::endl;
-    }
-
-    if (mallopt(M_MMAP_THRESHOLD, 0) == -1) {
-        std::cout << "Set M_MMAP_THRESHOLD failed" << std::endl;
-    }
-
-    if (mallopt(M_TRIM_THRESHOLD, 0) == -1) {
-        std::cout << "Set M_TRIM_THRESHOLD failed" << std::endl;
-    }
-}
-
-void memory_alloc_release()
-{
-    int count = 1000*1000;
-    u_int8_t data = 0xff;
-    u_int8_t** dataArray = new u_int8_t*[count];
-    for (int idx = 0; idx < count; idx++)
+    std::string serialized;
+    auto serialize_start = std::chrono::high_resolution_clock::now();
+    for (int idx = 0; idx < 1000; idx++)
     {
-        dataArray[idx] = new u_int8_t[1024];
-        memset(dataArray[idx], data, 1024);
-    }
-    
-    json j = json::parse("{}");
-    //auto connect = redisConnect("http://127.0.0.1", 6379);
-    //redisFree(connect);
-
-    for (int idx = 0; idx < count; idx++)
-    {
-        // check memory before delete
-        for (int pos = 0; pos < 1024; pos++)
+        // {"dest":"1.0.0.0/32","switch_id":"oid:0x1000000000001","vr":"oid:0x300000000"}
+        contacts::KeyOpFieldsValues bulkRequest;
+        bulkRequest.set_key("SAI_OBJECT_TYPE_ROUTE_ENTRY");
+        bulkRequest.set_op("SET");
+        for (int routes = 0; routes < 1000; routes++)
         {
-            if (dataArray[idx][pos] != data)
-            {
-                std::cout << "Data broken at: " << idx << "--" << pos << std::endl;
-            }
+            contacts::FieldsValues* route = bulkRequest.add_fvs();
+            route->set_field("{\"dest\":\"1.0.0.0/32\",\"switch_id\":\"oid:0x1000000000001\",\"vr\":\"oid:0x300000000\"}");
+            route->set_value("SAI_ROUTE_ENTRY_ATTR_PACKET_ACTION=SAI_PACKET_ACTION_FORWARD|SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID=oid:0x1000000000001");
         }
-
-        delete[] dataArray[idx];
+        bulkRequest.SerializeToString(&serialized);
     }
-    delete[] dataArray;
-
-    malloc_trim(0);
-    //MallocExtension::instance()->ReleaseFreeMemory();
+    auto serialize_duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - serialize_start);
+    serialize_duration_total+=serialize_duration.count();
+    auto deserialize_start = std::chrono::high_resolution_clock::now();
+    for (int idx = 0; idx < 1000; idx++)
+    {
+        contacts::KeyOpFieldsValues bulkRequest;
+        bulkRequest.ParseFromString(serialized);
+    }
+    auto deserialize_duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - deserialize_start);
+    deserialize_duration_total+=deserialize_duration.count();
+    // json:
+    std::vector<swss::FieldValueTuple> values;
+    auto field = "{\"dest\":\"1.0.0.0/32\",\"switch_id\":\"oid:0x1000000000001\",\"vr\":\"oid:0x300000000\"}";
+    auto value = "SAI_ROUTE_ENTRY_ATTR_PACKET_ACTION=SAI_PACKET_ACTION_FORWARD|SAI_ROUTE_ENTRY_ATTR_NEXT_HOP_ID=oid:0x1000000000001";
+    for (int routes = 0; routes < 1000; routes++)
+    {
+        values.emplace_back(field, value);
+    }
+    auto json_serialize_start = std::chrono::high_resolution_clock::now();
+    for (int idx = 0; idx < 1000; idx++)
+    {
+        serialized = swss::JSon::buildJson(values);
+    }
+    auto json_serialize_duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - json_serialize_start);
+    json_serialize_duration_total+=json_serialize_duration.count();
+    auto json_deserialize_start = std::chrono::high_resolution_clock::now();
+    for (int idx = 0; idx < 1000; idx++)
+    {
+        std::vector<swss::FieldValueTuple> fv;
+        swss::JSon::readJson(serialized, fv);
+    }
+    auto json_deserialize_duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - json_deserialize_start);
+    json_deserialize_duration_total+=json_deserialize_duration.count();
 }
 
 int main()
 {
-    //set_mallopt();
-    memory_alloc_release();
-    return exit_press_space();
+    serialize_duration_total = 0;
+    deserialize_duration_total = 0;
+    json_serialize_duration_total = 0;
+    json_deserialize_duration_total = 0;
+    int test_iteration = 100;
+    while(test_iteration > 0)
+    {
+        test();
+        printf("test_iteration: %d\n", test_iteration);
+        test_iteration--;
+    } 
+    printf("Serialize in ms: %ld\n", (serialize_duration_total / 100) / 1000);
+    printf("Deserialize in ms: %ld\n", (deserialize_duration_total / 100) / 1000);
+    printf("Json serialize in ms: %ld\n", (json_serialize_duration_total / 100) / 1000);
+    printf("Json deserialize in ms: %ld\n", (json_deserialize_duration_total / 100) / 1000);
 }
