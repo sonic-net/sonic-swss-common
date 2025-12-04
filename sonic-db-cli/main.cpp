8cb66f19807e6e396fae89019adaef84f0ad0bba
Sonic Build Admin <sonicbld@microsoft.com>
2025-12-04 19:59:12 +0000

    [sonic-db-cli] Add flag to make cli calls to container databases
    sonic-db-cli does not currently support calling the dpu redis instances that exist on smartswitches. This changes adds a -d/--dpu flag to select a dpu redis instance for running commands so that dpu instances can be targeted by the sonic-db-cli from the host OS

--- sonic-db-cli/main.cpp
+++ sonic-db-cli/main.cpp
@@ -7,16 +7,6 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    auto initializeGlobalConfig = []()
-    {
-        SonicDBConfig::initializeGlobalConfig(SonicDBConfig::DEFAULT_SONIC_DB_GLOBAL_CONFIG_FILE);
-    };
-
-    auto initializeConfig = []()
-    {
-        SonicDBConfig::initialize(SonicDBConfig::DEFAULT_SONIC_DB_CONFIG_FILE);
-    };
-
     return cli_exception_wrapper(
                     argc,
                     argv,
